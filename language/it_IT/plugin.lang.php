0527981ca533e839e1027f31cf016fe1739d2cca
riopwg <riopwg@piwigo.org>
2010-04-12 20:48:44 +0000

    Update language IT for gvideo
    git-svn-id: http://piwigo.org/svn/extensions/gvideo@5819 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/it_IT/plugin.lang.php
+++ language/it_IT/plugin.lang.php
@@ -1,45 +1,47 @@
 <?php
-$lang['py_title']  = 'PYWaie GVideo';
-$lang['py_conf']  = 'Configurazione';
-$lang['py_addvideo']  = 'Aggiungere una video';
-$lang['py_filename']  = 'Nome del file';
-$lang['py_url']  = 'indirizzo  della video';
-$lang['py_parent']  = 'Categoria parente';
-$lang['py_thumb']  = 'Miniatura';
-$lang['py_no_thumb']  = 'Nessuna Miniatura';
-$lang['py_thumb_from_server']  = 'Aggiungere la miniatura corrispondente sul server';
-$lang['py_thumb_from_user']  = 'Aggiungere la vostra miniatura ';
-$lang['py_add_band']  = 'Aggiungere  delle righe sui lati della miniatura';
-$lang['py_thumb_resize']  = 'Ridimensionare la miniatura';
-$lang['py_width']  = 'Larghezza';
-$lang['py_height']  = 'Altezza';
-$lang['py_start']  = 'Partenza della video';
-$lang['py_*']  = 'Campi obbligatori';
-$lang['py_autostart']  = 'Autostart';
-$lang['py_quality']  = 'Qualità';
-$lang['py_playermode']  = 'Player Mode';
+
+$lang['py_title'] = 'PYWaie GVideo';
+$lang['py_conf'] = 'Configurazione';
+$lang['py_addvideo'] = 'Aggiungere una video';
+$lang['py_filename'] = 'Nome del file';
+$lang['py_url'] = 'URL della video';
+$lang['py_parent'] = 'Categoria parente';
+$lang['py_thumb'] = 'Miniatura';
+$lang['py_no_thumb'] = 'Nessuna Miniatura';
+$lang['py_thumb_from_server'] = 'Aggiungere la miniatura corrispondente sul server';
+$lang['py_thumb_from_user'] = 'Aggiungere la vostra miniatura ';
+$lang['py_add_band'] = 'Aggiungere  delle righe sui lati della miniatura';
+$lang['py_thumb_resize'] = 'Ridimensionare la miniatura';
+$lang['py_width'] = 'Larghezza';
+$lang['py_height'] = 'Altezza';
+$lang['py_start'] = 'Partenza della video';
+$lang['py_*'] = 'Campi obbligatori';
+$lang['py_autostart'] = 'Autostart';
+$lang['py_quality'] = 'Qualità';
+$lang['py_playermode'] = 'Player Mode';
+
 
 // Infobulles
-$lang['pybulle_reference']  = 'Inserire qui il link della pagina dove si trova la video.<br> Potete usare delle video di Google, YouTube, DailyMotion, Wideo, Vimeo e Wat.
-Esempi: <br><br>- http://video.google.fr/videoplay?docid=4600755234172037942<br>- http://www.youtube.com/watch?v=eUvApC0bwUk<br>- http://www.dailymotion.com/video/x29fys_doorway<br>- http://www.wideo.fr/video/iLyROoaftYvl.html<br>- http://www.vimeo.com/1747316<br>- http://www.wat.tv/video/abcd.html';
-$lang['pybulle_start']  = 'definisce il punto di partenza (in secondi) della video. Questa opzione è attiva solo con Google Video.';
-$lang['pybulle_categorie']  = 'Categoria dove sarà scritto il file : solo le categorie reali vengono listate. Se si desidera avere il file in una categoria virtuale, occorrerà associarla in un secondo momento.';
-$lang['pybulle_miniature']  = 'Potete scegliere di recuperare la miniatura corrispondente alla video sul server. Di default, le dimensioni delle miniature sono 320x240 su Google Video, 320x240 su Dailymotion, 130x97 su Youtube, 154x114 su Wideo e 200x150 su Vimeo. <br><br>Potete anche scegliere di inviare la vostra miniatura. <br><br>In tutti i casi, potete eventualmente ridimensionarla';
-
+$lang['pybulle_reference'] = 'Inserire qui il link della pagina dove si trova la video.<br> Potete usare delle video di Google, YouTube, DailyMotion, Wideo, Vimeo e Wat. Esempi: <br><br>- http://video.google.fr/videoplay?docid=4600755234172037942<br>- http://www.youtube.com/watch?v=eUvApC0bwUk<br>- http://www.dailymotion.com/video/x29fys_doorway<br>- http://www.wideo.fr/video/iLyROoaftYvl.html<br>- http://www.vimeo.com/1747316<br>- http://www.wat.tv/video/abcd.html';
+$lang['pybulle_start'] = 'Definisce il punto di partenza (in secondi) della video. Questa opzione è attiva solo con Google Video.';
+$lang['pybulle_categorie'] = 'Categoria dove sarà scritto il file : solo le categorie reali vengono listate. Se si desidera avere il file in una categoria virtuale, occorrerà associarla in un secondo momento.';
+$lang['pybulle_miniature'] = 'Potete scegliere di recuperare la miniatura corrispondente alla video sul server. Di default, le dimensioni delle miniature sono 320x240 su Google Video, 320x240 su Dailymotion, 130x97 su Youtube, 154x114 su Wideo e 200x150 su Vimeo. <br><br>Potete anche scegliere di inviare la vostra miniatura. <br><br>In tutti i casi, potete eventualmente ridimensionarla';
 
 // Messages d'infos ou d'erreurs
-$lang['py_error2']  = 'Non è stato possibile procedere alla registrazione. Non avete riempito correttamente la scheda.';
-$lang['py_error3']  = 'Dovete completare tutti i campi obbligatori seniati con un\'asterisco';
-$lang['py_error5']  = 'Indirizzo non valido!';
-$lang['py_error6']  = 'Il file "%s" esiste già!';
-$lang['py_error7']  = 'Non è stato possibile registrare il file "%s"';
-$lang['py_error8']  = 'Verificare che la cartella %s possiede i permessi necessari (chmod).';
-$lang['py_error9']  = 'Non è stato possibile aggiungere o ridimensionare la miniatura.';
-$lang['py_error11']  = 'Una miniatura esiste già con questo nome (%s)';
-$lang['py_error12']  = 'Campo miniatura non completato: miniatura non aggiunta.';
-$lang['py_info2']  = 'Miniatura aggiunto con successo';
-$lang['py_info3']  = 'File registrato e sincronizzato con successo (%s)';
-$lang['py_info4']  = 'Configurazione salvata.';
-$lang['py_show_file']  = '<a href="%s">Visualizzare il file</a>.';
+$lang['py_error1'] = 'Nessun\' sito locale! Impossibile creare un file video.';
+$lang['py_error2'] = 'Non è stato possibile procedere alla registrazione. Non avete riempito correttamente la scheda.';
+$lang['py_error3'] = 'Dovete completare tutti i campi obbligatori seniati con un\'asterisco';
+$lang['py_error5'] = 'Indirizzo non valido!';
+$lang['py_error6'] = 'Il file "%s" esiste già!';
+$lang['py_error7'] = 'Non è stato possibile registrare il file "%s"';
+$lang['py_error8'] = 'Verificare che la cartella %s possiede i permessi necessari (chmod).';
+$lang['py_error9'] = 'Non è stato possibile aggiungere o ridimensionare la miniatura.';
+$lang['py_error11'] = 'Una miniatura esiste già con questo nome (%s)';
+$lang['py_error12'] = 'Campo miniatura non completato: miniatura non aggiunta.';
+$lang['py_info2'] = 'Miniatura aggiunto con successo';
+$lang['py_info3'] = 'File registrato e sincronizzato con successo (%s)';
+$lang['py_info4'] = 'Configurazione salvata.';
+$lang['py_show_file'] = '<a href="%s">Visualizzare il file</a>.';
+
 
 ?>
