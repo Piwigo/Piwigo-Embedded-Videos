1bb16e1819d642385d4468d6f455cb2aec0a5b75
ddtddt <ddtddt@piwigo.org>
2010-05-27 05:48:25 +0000

    [extension] gvideo - Add DE thanks to Sugar888
    git-svn-id: http://piwigo.org/svn/extensions/gvideo@6383 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/de_DE/plugin.lang.php
@@ -0,0 +1,48 @@
+<?php
+
+$lang['py_title'] = 'PYWaie GVideo';
+$lang['py_conf'] = 'Konfiguration';
+$lang['py_addvideo'] = 'Eine Videodatei hinzufügen';
+$lang['py_filename'] = 'Dateiname';
+$lang['py_url'] = 'URL des Videos';
+$lang['py_parent'] = 'Hauptkategorie';
+$lang['py_thumb'] = 'Miniaturbild';
+$lang['py_no_thumb'] = 'Kein Miniaturbild';
+$lang['py_thumb_from_server'] = 'Füge das Miniaturbild zum Server hinzu';
+$lang['py_thumb_from_user'] = 'Eigenes Miniaturbild hinzufügen:';
+$lang['py_add_band'] = 'Fügt Linien an den Seiten des Miniaturbildes hinzu';
+$lang['py_thumb_resize'] = 'Skaliert das Miniaturbild';
+$lang['py_width'] = 'Breite';
+$lang['py_height'] = 'Höhe';
+$lang['py_start'] = 'Start des Videos';
+$lang['py_*'] = 'Obligatorische Felder';
+$lang['py_autostart'] = 'Autostart';
+$lang['py_quality'] = 'Qualität';
+$lang['py_playermode'] = 'Modus des Players';
+
+
+// Infobulles
+
+$lang['pybulle_reference'] = "Hier den Link der Seite einfügen, in der das Video sich befindet.<br>Sie können Videos von GoogleVideo, YouTube, DailyMotion, Wideo, Vimeo oder Wat benutzen. Beispiele:<br><br>- http://video.google.fr/videoplay?docid=4600755234172037942<br>- http://www.youtube.com/watch?v=eUvApC0bwUk<br>- http://www.dailymotion.com/video/x29fys_doorway<br>- http://www.wideo.fr/video/iLyROoaftYvl.html<br>- http://www.vimeo.com/1747316<br>- http://www.wat.tv/video/abcd.html";
+$lang['pybulle_start'] = "Bestimmt den Anfang (in Sekunden) des Videos.<br>Diese Option kann nur mit GoogleVideo benutzt werden.";
+$lang['pybulle_categorie'] = "Kategorie, in welche die Datei erstellt wird: Es werden nur reale Kategorien aufgelistet. Falls man diese Datei in einer Virtuellen Kategorie haben möchte, kann man dies erst in einem späteren Verlauf konfigurieren.";
+$lang['pybulle_miniature'] = "Sie können bei Bedarf das Miniaturbild wieder auf dem  Server setzen. Als Standardeinstellung gelten die Grössen der Miniaturbilder 320x240 für Google Video, 320x240 für Dailymotion, 130x97 für Youtube, 154x114 für Wideo und 200x150 für Vimeo.<br><br> Sie haben auch die Möglichkeit Ihre eigene Miniaturbilder hochzuladen.<br><br> In beiden Fällen, können Sie die Grössen der Miniaturbilder bearbeiten.";
+
+
+// Messages d'infos ou d'erreurs
+$lang['py_error1'] = 'Keine lokale Seite! Es ist unmöglich eine Videodatei herzustellen.';
+$lang['py_error2'] = 'Die Informationen konnten nicht gespeichert werden. Sie haben das Formular nicht richtig ausgefüllt.';
+$lang['py_error3'] = 'Sie müssen die Obligatorischen Felder ausfüllen *';
+$lang['py_error5'] = 'Falsche URL!';
+$lang['py_error6'] = 'Datei "%s" existiert bereits.';
+$lang['py_error7'] = 'Die Datei konnte nicht erstellt werden "%s"';
+$lang['py_error8'] = 'Bitte überprüfen sie ob der Ordner %s die benötigten Rechte besitzt(chmod).';
+$lang['py_error9'] = 'Das Miniaturbild konnte nicht hinzugefügt oder die Grösse verändert werden .';
+$lang['py_error11'] = 'Es existiert bereits ein Miniaturbild mit dem gleichen Namen (%s)';
+$lang['py_error12'] = 'Das Feld des Miniaturbildes wurde nicht ausgefüllt : Das Miniaturbild wurde nicht hochgeladen.';
+$lang['py_info2'] = 'Das Miniaturbild wurde erfolgreich hochgeladen';
+$lang['py_info3'] = 'Die Datei wurde erfolgreich gespeichert und synchronisiert (%s)';
+$lang['py_info4'] = 'Einstellungen gespeichert.';
+$lang['py_show_file'] = '<a href="%s">Datei anzeigen</a>.';
+
+?>
