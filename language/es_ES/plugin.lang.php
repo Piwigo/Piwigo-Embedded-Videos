1975b6735d0dd0239c71318de2dd2d7b6badbac0
ddtddt <ddtddt@piwigo.org>
2010-10-20 14:33:10 +0000

    [extensions] - gvideo - add ES thanks to jpr
    git-svn-id: http://piwigo.org/svn/extensions/gvideo@7305 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/es_ES/plugin.lang.php
@@ -0,0 +1,47 @@
+<?php
+
+$lang['py_title'] = 'PYWaie GVideo';
+$lang['py_conf'] = 'Configuración';
+$lang['py_addvideo'] = 'Añadir video';
+$lang['py_filename'] = 'Nombre del fichero';
+$lang['py_url'] = 'URL de la video';
+$lang['py_parent'] = 'Categoria pariente ';
+$lang['py_thumb'] = 'Miniatura';
+$lang['py_no_thumb'] = 'Sin miniatura';
+$lang['py_thumb_from_server'] = 'Cargar la miniatura corespondiente en el servidor';
+$lang['py_thumb_from_user'] = 'Cargar su propria miniatura';
+$lang['py_add_band'] = 'Añadir trias en los lados de la miniatura';
+$lang['py_thumb_resize'] = 'Cambiar el tamaño de la miniatura';
+$lang['py_width'] = 'Ancho';
+$lang['py_height'] = 'Altura';
+$lang['py_start'] = 'Arranque de la video';
+$lang['py_*'] = 'Campos obligatorios';
+$lang['py_autostart'] = 'Auto arranquet';
+$lang['py_quality'] = 'Calidad';
+$lang['py_playermode'] = 'Modo Player';
+
+
+// Infobulles
+$lang['pybulle_reference'] = "Pegar aquí el enlace a la página donde se encuentra el vídeo.<br>Usted puede utilizar vídeos de Google, YouTube, DailyMotion, Wideo, Vimeo et Wat. Ejemplos:<br><br>- http://video.google.fr/videoplay?docid=4600755234172037942<br>- http://www.youtube.com/watch?v=eUvApC0bwUk<br>- http://www.dailymotion.com/video/x29fys_doorway<br>- http://www.wideo.fr/video/iLyROoaftYvl.html<br>- http://www.vimeo.com/1747316<br>- http://www.wat.tv/video/abcd.html";
+$lang['pybulle_start'] = "Define el punto de arranque (en segundos) de la vídeo.<br>Esta opción sólo está activa con Google Vidéo.";
+$lang['pybulle_categorie'] = "Categoria donde estara escrito el fichero: sólo las categorías reales figuran en esta lista. Si desea que el archivo figura en una categoría virtual, tendra que asociarlo despues.";
+$lang['pybulle_miniature'] = "Usted puede eligir de recuperar la miniatura corespondiente a la video sur le serveur.Por defecto el tamaño de las miniaturas es de 320x240 en Google Vidéo, 320x240 en Dailymotion, 130x97 en Youtube, 154x114 en Wideo y 200x150 en Vimeo.<br><br>También puede optar por subir su propia miniatura.<br><br>En todos los casos, usted puede cambiar el tamaño.";
+
+// Messages d'infos ou d'erreurs
+$lang['py_error1'] = 'Ningún sitio web local! No se puede crear un archivo de vídeo.';
+$lang['py_error2'] = 'No se puede proceder al registro. Usted no ha llenado corectamente el formulario.';
+$lang['py_error3'] = 'Debe completar los campos obligatorios marcados *';
+$lang['py_error5'] = 'URL invalida!';
+$lang['py_error6'] = 'El fichero "%s" ya existe.';
+$lang['py_error7'] = 'Imposible de escribir el fichero "%s"';
+$lang['py_error8'] = 'Por favor, compruebe que el archivo% s tiene los permisos necesarios (chmod).';
+$lang['py_error9'] = 'No se puede  cargar o cambiar el tamaño de la miniatura.';
+$lang['py_error11'] = 'Una miniatura ya existe con ese nombre (%s)';
+$lang['py_error12'] = 'No se ha especificado un nombre para la miniatura : miniatura no cargada.';
+$lang['py_info2'] = 'Miniatura subida con éxito.';
+$lang['py_info3'] = 'Video añadido';
+$lang['py_info4'] = 'Configuración guardada.';
+$lang['py_show_file'] = '<a href="%s" class="externalLink">Ver la video</a>.';
+
+
+?>
