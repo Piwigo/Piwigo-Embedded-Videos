9b1eb40434dd4f17fbc35252ff75d4b09023f1e6
ddtddt <ddtddt@piwigo.org>
2010-10-22 16:50:18 +0000

    [extensions] - gvideo add ru thanks to yuragal
    git-svn-id: http://piwigo.org/svn/extensions/gvideo@7343 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/ru_RU/plugin.lang.php
@@ -0,0 +1,46 @@
+<?php
+
+$lang['py_title'] = 'PYWaie GVideo';
+$lang['py_conf'] = 'Настройки';
+$lang['py_addvideo'] = 'Добавить видео-ролик';
+$lang['py_filename'] = 'Имя файла';
+$lang['py_url'] = 'URL видео-ролика';
+$lang['py_parent'] = 'Основная категория';
+$lang['py_thumb'] = 'Миниатюра';
+$lang['py_no_thumb'] = 'Миниатюра отсутствует';
+$lang['py_thumb_from_server'] = 'Загрузить миниатюру с сервера';
+$lang['py_thumb_from_user'] = 'Загрузить пользовательскую миниатюру:';
+$lang['py_add_band'] = 'Добавить полосы по краям миниатюры';
+$lang['py_thumb_resize'] = 'Изменить размер миниатюры';
+$lang['py_width'] = 'Ширина';
+$lang['py_height'] = 'Высота';
+$lang['py_start'] = 'Начало воспроизведения видео-ролика';
+$lang['py_*'] = 'Обязательное поле';
+$lang['py_autostart'] = 'Автовоспроизведение';
+$lang['py_quality'] = 'Качество';
+$lang['py_playermode'] = 'Режим воспроизведения';
+
+
+// Подсказки
+$lang['pybulle_reference'] = "Ссылка на страницу с видео.<br>Доступные видео-сервисы: GoogleVideo, YouTube, DailyMotion, Wideo, Vimeo или Wat. Примеры:<br><br>- http://video.google.fr/videoplay?docid=4600755234172037942<br>- http://www.youtube.com/watch?v=eUvApC0bwUk<br>- http://www.dailymotion.com/video/x29fys_doorway<br>- http://www.wideo.fr/video/iLyROoaftYvl.html<br>- http://www.vimeo.com/1747316<br>- http://www.wat.tv/video/abcd.html";
+$lang['pybulle_start'] = "Укажите точку начала воспроизведения видео-ролика (в секундах).<br>Эта опция доступна только для роликов с сайта GoogleVideo.";
+$lang['pybulle_categorie'] = 'Категория, в которой будет создан файл: перечислены только "категории-каталоги". Если вы хотите связать файл с виртуальной категорией, это можно будет сделать после добавления видео-ролика.';
+$lang['pybulle_miniature'] = "Вы можете получить миниатюру с видео-сервиса. По умолчанию размеры миниатюр следующие: 320x240 для Google Video, 320x240 для Dailymotion, 130x97 для Youtube, 154x114 для Wideo и 200x150 для Vimeo.<br><br> Также вы можете загрузить свою собственную миниатюру.<br><br>В любом случае, вы можете изменить размеры миниатюры.";
+
+// Информационные сообщения и ошибки
+$lang['py_error1'] = 'Нет локальной директории! Невозможно создать видео-файл.';
+$lang['py_error2'] = 'Ошибка при сохранении. Неверно заполнена форма.';
+$lang['py_error3'] = 'Заполните обязательные поля *.';
+$lang['py_error5'] = 'Неверный адрес URL!';
+$lang['py_error6'] = 'Файл "%s" уже существует.';
+$lang['py_error7'] = 'Невозможно записать файл "%s"';
+$lang['py_error8'] = 'Пожалуйста, проверьте права доступа к директории %s (chmod).';
+$lang['py_error9'] = 'Ошибка при загрузке/изменении размера миниатюры.';
+$lang['py_error11'] = 'Миниатюра с таким именем уже существует (%s)';
+$lang['py_error12'] = 'Поле "Миниатюра" не заполнено: миниатюра не загружена.';
+$lang['py_info2'] = 'Миниатюра успешно загружена.';
+$lang['py_info3'] = 'Видео-ролик успешно добавлен.';
+$lang['py_info4'] = 'Настройки сохнанены.';
+$lang['py_show_file'] = '<a href="%s" class="externalLink">Просмотреть видео-ролик</a>.';
+
+?>
