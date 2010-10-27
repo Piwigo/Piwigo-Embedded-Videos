dd365de973b62edbee0357041ba7e389cff96cd1
ddtddt <ddtddt@piwigo.org>
2010-10-27 11:03:48 +0000

    [extensions] - gvideo update PL bug encoding
    git-svn-id: http://piwigo.org/svn/extensions/gvideo@7429 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/pl_PL/plugin.lang.php
+++ language/pl_PL/plugin.lang.php
@@ -5,42 +5,42 @@ $lang['py_conf'] = 'Konfiguracja';
 $lang['py_addvideo'] = 'Dodaj film';
 $lang['py_filename'] = 'Nazwa pliku';
 $lang['py_url'] = 'Adres URL filmu';
-$lang['py_parent'] = 'G³ówna kategoria';
+$lang['py_parent'] = 'GÂ³Ã³wna kategoria';
 $lang['py_thumb'] = 'Miniatura';
 $lang['py_no_thumb'] = 'Brak miniatury';
-$lang['py_thumb_from_server'] = 'Wgraj miniaturê z serwera';
-$lang['py_thumb_from_user'] = 'Wgraj w³asn¹ miniaturê:';
+$lang['py_thumb_from_server'] = 'Wgraj miniaturÃª z serwera';
+$lang['py_thumb_from_user'] = 'Wgraj wÂ³asnÂ¹ miniaturÃª:';
 $lang['py_add_band'] = 'Dodaj paski po obu stronach miniatury';
-$lang['py_thumb_resize'] = 'Zmieñ rozmiar miniatury';
-$lang['py_width'] = 'Szerokoœæ';
-$lang['py_height'] = 'Wysokoœæ';
-$lang['py_start'] = 'Pocz¹tek filmu';
-$lang['py_*'] = 'Pole obowi¹zkowe';
+$lang['py_thumb_resize'] = 'ZmieÃ± rozmiar miniatury';
+$lang['py_width'] = 'SzerokoÅ“Ã¦';
+$lang['py_height'] = 'WysokoÅ“Ã¦';
+$lang['py_start'] = 'PoczÂ¹tek filmu';
+$lang['py_*'] = 'Pole obowiÂ¹zkowe';
 $lang['py_autostart'] = 'Autostart';
-$lang['py_quality'] = 'Jakoœæ';
+$lang['py_quality'] = 'JakoÅ“Ã¦';
 $lang['py_playermode'] = 'Tryb odtwarzacza';
 
 
 // Infobulles
-$lang['pybulle_reference'] = "Wklej tutaj adres strony z filmem.<br>Mo¿esz u¿yæ GoogleVideo, YouTube, DailyMotion, Wideo, Vimeo lub Wat. Przyk³ady:<br><br>- http://video.google.fr/videoplay?docid=4600755234172037942<br>- http://www.youtube.com/watch?v=eUvApC0bwUk<br>- http://www.dailymotion.com/video/x29fys_doorway<br>- http://www.wideo.fr/video/iLyROoaftYvl.html<br>- http://www.vimeo.com/1747316<br>- http://www.wat.tv/video/abcd.html";
-$lang['pybulle_start'] = "Wybierz punkt startu filmu (w sekundach).<br>Ta opcja mo¿e byæ wykorzstana tylko z GoogleVideo.";
-$lang['pybulle_categorie'] = "Kategoria, wktórej plik zostanie stworzony: wyœwietlane s¹ tylko prawdziwe kategorie. Je¿eli chcesz dodaæ link tego pliku do kategorii wirtualnej, mo¿esz to zrobiæ póŸniej.";
-$lang['pybulle_miniature'] = "Mo¿esz trzymaæ plik miniatury na serwerze. Domuœlnie rozmiary miniatur s¹ nastêpuj¹ce: 320x240 dla Google Video, 320x240 dla Dailymotion, 130x97 dla Youtube, 154x114 dla Wideo aoraz 200x150 dla Vimeo.<br><br> Mo¿esz tak¿e wgraæ w³asn¹ miniaturê.<br><br> W obu przypadkach, mo¿esz zmieniæ rozmiary miinatury.";
+$lang['pybulle_reference'] = "Wklej tutaj adres strony z filmem.<br>MoÂ¿esz uÂ¿yÃ¦ GoogleVideo, YouTube, DailyMotion, Wideo, Vimeo lub Wat. PrzykÂ³ady:<br><br>- http://video.google.fr/videoplay?docid=4600755234172037942<br>- http://www.youtube.com/watch?v=eUvApC0bwUk<br>- http://www.dailymotion.com/video/x29fys_doorway<br>- http://www.wideo.fr/video/iLyROoaftYvl.html<br>- http://www.vimeo.com/1747316<br>- http://www.wat.tv/video/abcd.html";
+$lang['pybulle_start'] = "Wybierz punkt startu filmu (w sekundach).<br>Ta opcja moÂ¿e byÃ¦ wykorzstana tylko z GoogleVideo.";
+$lang['pybulle_categorie'] = "Kategoria, wktÃ³rej plik zostanie stworzony: wyÅ“wietlane sÂ¹ tylko prawdziwe kategorie. JeÂ¿eli chcesz dodaÃ¦ link tego pliku do kategorii wirtualnej, moÂ¿esz to zrobiÃ¦ pÃ³Å¸niej.";
+$lang['pybulle_miniature'] = "MoÂ¿esz trzymaÃ¦ plik miniatury na serwerze. DomuÅ“lnie rozmiary miniatur sÂ¹ nastÃªpujÂ¹ce: 320x240 dla Google Video, 320x240 dla Dailymotion, 130x97 dla Youtube, 154x114 dla Wideo aoraz 200x150 dla Vimeo.<br><br> MoÂ¿esz takÂ¿e wgraÃ¦ wÂ³asnÂ¹ miniaturÃª.<br><br> W obu przypadkach, moÂ¿esz zmieniÃ¦ rozmiary miinatury.";
 
 // Messages d'infos ou d'erreurs
-$lang['py_error1'] = 'Brak strony lokalnej! Nie mo¿na stworzyæ pliku z filmem.';
-$lang['py_error2'] = 'B³¹d zapisu. Nie uzupe³niono poprawnie pól formularza.';
-$lang['py_error3'] = 'Musisz uzupe³niæ wszystkie obowi¹zkowe pola oznacozne *';
-$lang['py_error5'] = 'B³êdny adres URL!';
-$lang['py_error6'] = 'plik "%s" ju¿ istnieje.';
-$lang['py_error7'] = 'B³¹d zapisu pliku "%s"';
-$lang['py_error8'] = 'SprawdŸ proszê czy katalog %s ma odpowiednie uprawnienia(chmod).';
-$lang['py_error9'] = 'B³¹d podczas wgrywania miniatury i zmiany jej rozmiaru.';
-$lang['py_error11'] = 'Miniatura o tej samej nazwie ju¿ istnieje (%s)';
-$lang['py_error12'] = 'pole miniatury nie wype³nione : miniatura nie zosta³a wgrana.';
-$lang['py_info2'] = 'miniatura zosta³a wgrana pomyœlnie';
-$lang['py_info3'] = 'Film dodany pomyœlnie';
+$lang['py_error1'] = 'Brak strony lokalnej! Nie moÂ¿na stworzyÃ¦ pliku z filmem.';
+$lang['py_error2'] = 'BÂ³Â¹d zapisu. Nie uzupeÂ³niono poprawnie pÃ³l formularza.';
+$lang['py_error3'] = 'Musisz uzupeÂ³niÃ¦ wszystkie obowiÂ¹zkowe pola oznacozne *';
+$lang['py_error5'] = 'BÂ³Ãªdny adres URL!';
+$lang['py_error6'] = 'plik "%s" juÂ¿ istnieje.';
+$lang['py_error7'] = 'BÂ³Â¹d zapisu pliku "%s"';
+$lang['py_error8'] = 'SprawdÅ¸ proszÃª czy katalog %s ma odpowiednie uprawnienia(chmod).';
+$lang['py_error9'] = 'BÂ³Â¹d podczas wgrywania miniatury i zmiany jej rozmiaru.';
+$lang['py_error11'] = 'Miniatura o tej samej nazwie juÂ¿ istnieje (%s)';
+$lang['py_error12'] = 'pole miniatury nie wypeÂ³nione : miniatura nie zostaÂ³a wgrana.';
+$lang['py_info2'] = 'miniatura zostaÂ³a wgrana pomyÅ“lnie';
+$lang['py_info3'] = 'Film dodany pomyÅ“lnie';
 $lang['py_info4'] = 'Konfiguracja zapisana.';
-$lang['py_show_file'] = '<a href="%s" class="externalLink">SPoka¿ film</a>.';
+$lang['py_show_file'] = '<a href="%s" class="externalLink">SPokaÂ¿ film</a>.';
 
 ?>
