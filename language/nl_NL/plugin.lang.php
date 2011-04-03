6fc58dc7ab7e333eb5fcafee7ae139b25a72ac40
ddtddt <ddtddt@piwigo.org>
2011-04-03 07:49:53 +0000

    [extensions] - gvideo - add nl_NL thanks to J.Commelin
    git-svn-id: http://piwigo.org/svn/extensions/gvideo@10004 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/nl_NL/plugin.lang.php
@@ -0,0 +1,45 @@
+<?php
+
+$lang['py_title'] = 'PYWaie GVideo';
+$lang['py_conf'] = 'Configuratie';
+$lang['py_addvideo'] = 'Voeg video toe';
+$lang['py_filename'] = 'Bestandsnaam';
+$lang['py_url'] = 'URL van de video';
+$lang['py_parent'] = 'Hoofd Categorie';
+$lang['py_thumb'] = 'Thumbnail';
+$lang['py_no_thumb'] = 'Geen thumbnail';
+$lang['py_thumb_from_server'] = 'Upload thumbnail van server';
+$lang['py_thumb_from_user'] = 'Upload uw eigen thumbnail:';
+$lang['py_add_band'] = 'Voeg randen toe op de zijden van de thumbnail';
+$lang['py_thumb_resize'] = 'Schaal de thumbnail';
+$lang['py_width'] = 'Breedte';
+$lang['py_height'] = 'Hoogte';
+$lang['py_start'] = 'Begin van de video';
+$lang['py_*'] = 'Verplicht invoerveld';
+$lang['py_autostart'] = 'Autostart';
+$lang['py_quality'] = 'Kwaliteit';
+$lang['py_playermode'] = 'Afspeelmodus';
+
+// Infobulles
+$lang['pybulle_reference'] = "Plaats hier een link naar de video.<br>Je kunt gebruik maken van GoogleVideo, YouTube, DailyMotion, Wideo, Vimeo of Wat. Voorbeelden:<br><br>- http://video.google.fr/videoplay?docid=4600755234172037942<br>- http://www.youtube.com/watch?v=eUvApC0bwUk<br>- http://www.dailymotion.com/video/x29fys_doorway<br>- http://www.wideo.fr/video/iLyROoaftYvl.html<br>- http://www.vimeo.com/1747316<br>- http://www.wat.tv/video/abcd.html";
+$lang['pybulle_start'] = "Kies het startpunt (in seconden) van de video.<br>Deze optie werkt alleen met GoogleVideo.";
+$lang['pybulle_categorie'] = 'Categorie waar het bestand wordt aangemaakt: alleen echte categorie"en worden getoond. Als u dit bestand wilt linken aan een virtuele categorie zult u dat later moeten doen.';
+$lang['pybulle_miniature'] = "U kunt de thumbnail van de server krijgen. Per standaard zijn de thumbnails 320x240 op Google Video, 320x240 op Dailymotion, 130x97 op Youtube, 154x114 op Wideo en 200x150 op Vimeo.<br><br> U kunt ook uw eigen thumbnail uploaden.<br><br> In beide gevallen kunt u de grootte van de thumbnail aanpassen.";
+
+// Messages d'infos ou d'erreurs
+$lang['py_error1'] = 'Geen lokale site! Niet mogelijk om een video bestand aan te maken.';
+$lang['py_error2'] = 'Opslaan niet mogelijk. Het formulier is niet juist ingevuld.';
+$lang['py_error3'] = 'U moet verplichte verlden invullen *';
+$lang['py_error5'] = 'verkeerde URL!';
+$lang['py_error6'] = 'bestand "%s" bestaat al.';
+$lang['py_error7'] = 'Niet mogelijk om betand "%s" te schrijven';
+$lang['py_error8'] = 'controleer of de map %s rechten heeft(chmod).';
+$lang['py_error9'] = 'Fout tijdens het uploaden of schalen van de thumbnail.';
+$lang['py_error11'] = 'Er bestaat al een thumbnail met dezelfde naam (%s)';
+$lang['py_error12'] = 'Thumbnail niet ingevuld : thumbnail niet geupload.';
+$lang['py_info2'] = 'Thumbnail succesvol geupload';
+$lang['py_info3'] = 'Video succesvol toegevoegd';
+$lang['py_info4'] = 'Configuratie opgeslagen.';
+$lang['py_show_file'] = '<a href="%s" class="externalLink">Toon video</a>.';
+
+?>
