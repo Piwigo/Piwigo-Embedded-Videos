cef97846d09d6ec064288629c946fbc0257d89b1
ddtddt <ddtddt@piwigo.org>
2011-04-13 04:42:40 +0000

    [extensions] - gvideo - add zh_CN Thanks to maramail - Step 1
    git-svn-id: http://piwigo.org/svn/extensions/gvideo@10345 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/zh_CN/plugin.lang.php
@@ -0,0 +1,24 @@
+<?php
+
+$lang['py_title'] = 'PYWaie GVideo';
+$lang['py_conf'] = '设置';
+$lang['py_addvideo'] = '添加一段视频';
+$lang['py_filename'] = '文件名';
+$lang['py_url'] = '视频地址';
+$lang['py_parent'] = '主类别';
+$lang['py_thumb'] = '预览图';
+$lang['py_no_thumb'] = '无预览图';
+$lang['py_thumb_from_server'] = '从服务器上传预览图';
+$lang['py_thumb_from_user'] = '上传你自己的预览图';
+$lang['py_add_band'] = '在预览图旁边添加标志';
+$lang['py_thumb_resize'] = '调整预览图大小';
+$lang['py_width'] = '宽度';
+$lang['py_height'] = '高度';
+$lang['py_start'] = '视频开始处';
+$lang['py_*'] = '必填项';
+$lang['py_autostart'] = '自动开始';
+$lang['py_quality'] = '质量';
+$lang['py_playermode'] = '播放器模式';
+
+
+?>
