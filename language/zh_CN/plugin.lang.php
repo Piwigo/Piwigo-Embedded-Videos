1ea7ea3e28be5af89c908933f687f99b0a722efd
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-03-07 13:34:10 +0000

    [GVideo] Update zh_CN, thanks to : quqibing
    git-svn-id: http://piwigo.org/svn/extensions/gvideo@13508 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/zh_CN/plugin.lang.php
+++ language/zh_CN/plugin.lang.php
@@ -21,4 +21,22 @@ $lang['py_quality'] = '质量';
 $lang['py_playermode'] = '播放器模式';
 
 
+$lang['pybulle_categorie'] = '生成文件的目录: 只列出真实目录. 如果想要链接文件到虚拟目录, 你可以之后进行链接.';
+$lang['pybulle_miniature'] = '你可以从服务器上获取缩略图. 默认缩略图大小Google Video是320x240, Dailymotion是320x240, Youtube是130x97, Wideo是154x114,Vimeo是200x150.<br><br> 你也可以上传自己的缩略图.<br><br> 在这两种情况下，你都可以更改缩略图的尺寸。';
+$lang['pybulle_reference'] = '这里粘贴视频链接.<br>支持GoogleVideo, YouTube, DailyMotion, Wideo, Vimeo or Wat. 例如:<br><br>- http://video.google.fr/videoplay?docid=4600755234172037942<br>- http://www.youtube.com/watch?v=eUvApC0bwUk<br>- http://www.dailymotion.com/video/x29fys_doorway<br>- http://www.wideo.fr/video/iLyROoaftYvl.html<br>- http://www.vimeo.com/1747316<br>- http://www.wat.tv/video/abcd.html';
+$lang['pybulle_start'] = '选择视频的开始位置(秒).<br>这个选项只对GoogleVideo生效';
+$lang['py_error11'] = '已存在同名缩略图文件((%s)';
+$lang['py_error12'] = '没有指定缩略图：缩略图未被上传。';
+$lang['py_error1'] = '没有本地网站! 无法创建视频文件.';
+$lang['py_error2'] = '无法保存. 填写的格式不正确.';
+$lang['py_error3'] = '必须填写必填字段*';
+$lang['py_error5'] = 'URL错误!';
+$lang['py_error6'] = '文件"%s"已经存在.';
+$lang['py_error7'] = '无法写入文件"%s"';
+$lang['py_error8'] = '请检查目录%s是否有足够权限(chmod)';
+$lang['py_error9'] = '更新或缩放缩略图时出错.';
+$lang['py_info2'] = '缩略图上传成功';
+$lang['py_info3'] = '视频加载成功';
+$lang['py_info4'] = '配置已保存';
+$lang['py_show_file'] = '<a href="%s" class="externalLink">显示视频</a>.';
 ?>
