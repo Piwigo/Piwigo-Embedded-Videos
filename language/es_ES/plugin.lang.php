f7af73384b21b0a6a65b07dc8594347b82efe780
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-05-03 19:25:30 +0000

    [gvideo] Update es_ES, thanks to : AlbertParera
    git-svn-id: http://piwigo.org/svn/extensions/gvideo@14727 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/es_ES/plugin.lang.php
+++ language/es_ES/plugin.lang.php
@@ -22,14 +22,14 @@ $lang['py_playermode'] = 'Modo Player';
 
 
 // Infobulles
-$lang['pybulle_reference'] = "Pegar aquí el enlace a la página donde se encuentra el vídeo.<br>Usted puede utilizar vídeos de Google, YouTube, DailyMotion, Wideo, Vimeo et Wat. Ejemplos:<br><br>- http://video.google.fr/videoplay?docid=4600755234172037942<br>- http://www.youtube.com/watch?v=eUvApC0bwUk<br>- http://www.dailymotion.com/video/x29fys_doorway<br>- http://www.wideo.fr/video/iLyROoaftYvl.html<br>- http://www.vimeo.com/1747316<br>- http://www.wat.tv/video/abcd.html";
+$lang['pybulle_reference'] = 'Pegar aquí el enlace a la página donde se encuentra el vídeo.<br>Usted puede utilizar vídeos de Google, YouTube, DailyMotion, Wideo, Vimeo o Wat. Ejemplos:<br><br>- http://video.google.fr/videoplay?docid=4600755234172037942<br>- http://www.youtube.com/watch?v=eUvApC0bwUk<br>- http://www.dailymotion.com/video/x29fys_doorway<br>- http://www.wideo.fr/video/iLyROoaftYvl.html<br>- http://www.vimeo.com/1747316<br>- http://www.wat.tv/video/abcd.html';
 $lang['pybulle_start'] = "Define el punto de arranque (en segundos) de la vídeo.<br>Esta opción sólo está activa con Google Vidéo.";
 $lang['pybulle_categorie'] = "Categoria donde estara escrito el fichero: sólo las categorías reales figuran en esta lista. Si desea que el archivo figura en una categoría virtual, tendra que asociarlo despues.";
 $lang['pybulle_miniature'] = "Usted puede eligir de recuperar la miniatura corespondiente a la video sur le serveur.Por defecto el tamaño de las miniaturas es de 320x240 en Google Vidéo, 320x240 en Dailymotion, 130x97 en Youtube, 154x114 en Wideo y 200x150 en Vimeo.<br><br>También puede optar por subir su propia miniatura.<br><br>En todos los casos, usted puede cambiar el tamaño.";
 
 // Messages d'infos ou d'erreurs
 $lang['py_error1'] = 'Ningún sitio web local! No se puede crear un archivo de vídeo.';
-$lang['py_error2'] = 'No se puede proceder al registro. Usted no ha llenado corectamente el formulario.';
+$lang['py_error2'] = 'No se puede proceder al registro. No no ha llenado correctamente el formulario.';
 $lang['py_error3'] = 'Debe completar los campos obligatorios marcados *';
 $lang['py_error5'] = 'URL invalida!';
 $lang['py_error6'] = 'El fichero "%s" ya existe.';
@@ -39,7 +39,7 @@ $lang['py_error9'] = 'No se puede  cargar o cambiar el tamaño de la miniatura.'
 $lang['py_error11'] = 'Una miniatura ya existe con ese nombre (%s)';
 $lang['py_error12'] = 'No se ha especificado un nombre para la miniatura : miniatura no cargada.';
 $lang['py_info2'] = 'Miniatura subida con éxito.';
-$lang['py_info3'] = 'Video añadido';
+$lang['py_info3'] = 'Vídeo añadido';
 $lang['py_info4'] = 'Configuración guardada.';
 $lang['py_show_file'] = '<a href="%s" class="externalLink">Ver la video</a>.';
 
