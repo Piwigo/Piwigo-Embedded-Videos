1841e72a4de02c46e0d1a7b181e9fc0cada4387e
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-05-03 19:25:23 +0000

    [gvideo] Update ca_ES, thanks to : AlbertParera
    git-svn-id: http://piwigo.org/svn/extensions/gvideo@14726 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/ca_ES/plugin.lang.php
+++ language/ca_ES/plugin.lang.php
@@ -48,4 +48,13 @@ $lang['py_info2'] = 'La miniatura s\'ha pujat correctament';
 $lang['py_info3'] = 'El vídeo s\'ha afegit correctament';
 $lang['py_info4'] = 'Configuració guardada.';
 $lang['py_error9'] = 'Error al pujar o redimensionar una miniatura.';
+$lang['py_error1'] = 'No hi ha cap lloc web local. No es pot crear cap arxiu de vídeo.';
+$lang['py_error6'] = 'L\'arxiu "%s" ja existeix.';
+$lang['py_error7'] = 'No es pot escriure a l\'arxiu "%s"';
+$lang['py_error8'] = 'Si us plau, comprovi que el directori %s disposa de permisos (chmod).';
+$lang['py_show_file'] = '<a href="%s" class="externalLink">Veure vídeo</a>.';
+$lang['pybulle_categorie'] = 'Àlbum en el qual es crea l\'arxiu. Només s\'enumeren els àlbums físics. En cas de voler vincular aquest arxiu a un àlbum virtual, caldria fer-ho després.';
+$lang['pybulle_miniature'] = 'Per una banda, pot obtenir i recuperar les miniatura de nou al servidor. Per defecte, les mides de les miniatures són de 320x240 a GoogleVideo, 320x240 a Dailymotion, 130x97 a Youtube, 154x114 a Wideo i 200x150 a Vimeo.<br><br>Per altra banda, també pot pujar les seves pròpies miniatures.<br><br>En ambdós casos, en podeu canviar les mides.';
+$lang['pybulle_reference'] = 'Enganxi aquí la URL d\'enllaç a la pàgina on hi hagi el vídeo.<br>Es poden afegir vídeos de GoogleVideo, YouTube, DailyMotion, Wideo, Vimeo o Wat. Exemples:<br><br>- http://video.google.fr/videoplay?docid=4600755234172037942<br>- http://www.youtube.com/watch?v=eUvApC0bwUk<br>- http://www.dailymotion.com/video/x29fys_doorway<br>- http://www.wideo.fr/video/iLyROoaftYvl.html<br>- http://www.vimeo.com/1747316<br>- http://www.wat.tv/video/abcd.html';
+$lang['pybulle_start'] = 'Defineix el punt d\'inici (en segons) del vídeo.<br>Aquesta opció està només disponible per a GoogleVideo.';
 ?>
