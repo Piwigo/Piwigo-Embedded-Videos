97618fd0610d9a4256a06e8861be0abeb9b5b7f9
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-05-03 14:44:03 +0000

    [gvideo] Update ca_ES, thanks to : AlbertParera
    git-svn-id: http://piwigo.org/svn/extensions/gvideo@14713 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/ca_ES/plugin.lang.php
@@ -0,0 +1,43 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['py_*'] = 'Camp obligatori';
+$lang['py_add_band'] = 'Afegir bandes en els costats de la imatge';
+$lang['py_addvideo'] = 'Afegir un vídeo';
+$lang['py_autostart'] = 'Autoiniciar';
+$lang['py_conf'] = 'Configuració';
+$lang['py_error5'] = 'URL errònia.';
+$lang['py_filename'] = 'Nom de l\'arxiu';
+$lang['py_height'] = 'Altura';
+$lang['py_no_thumb'] = 'sense miniatures';
+$lang['py_parent'] = 'Categoria Principal';
+$lang['py_playermode'] = 'Mode Reproductor';
+$lang['py_quality'] = 'Qualitat';
+$lang['py_start'] = 'Inici del vídeo';
+$lang['py_thumb_from_server'] = 'Carregar miniatura al servidor';
+$lang['py_thumb_from_user'] = 'Carregar miniatura';
+$lang['py_thumb_resize'] = 'Canviar la mida de la imatge';
+$lang['py_thumb'] = 'Miniatura';
+$lang['py_title'] = 'PYWaie GVideo';
+$lang['py_url'] = 'URL del vídeo';
+$lang['py_width'] = 'Amplada';
+?>
