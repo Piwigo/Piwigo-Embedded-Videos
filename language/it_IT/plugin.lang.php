85d4cff30efc0ebcc7998c31966a4e69bae26d03
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-08-02 21:56:13 +0000

    [gvideo] Update it_IT, thanks to :
    git-svn-id: http://piwigo.org/svn/extensions/gvideo@17339 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/it_IT/plugin.lang.php
@@ -0,0 +1,43 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Video successfully added. <a href="%s">View</a>'] = 'Video aggiunto con successo. <a href="%s">Vedi</a>';
+$lang['Use common setting'] = 'Utilizzare un\'impostazione comune';
+$lang['Video URL'] = 'URL video';
+$lang['Video size'] = 'Dimensione video';
+$lang['Unable to contact host server'] = 'Impossibile contattare il server host';
+$lang['This element is a video added with "Embedded Video"'] = 'Questo elemento è aggiunto un video con "Embedded Video"';
+$lang['Supported services'] = 'Servizi supportati';
+$lang['Please fill the video URL'] = 'Si prega di inserire l\'URL del video';
+$lang['Please fill the video name'] = 'Si prega di inserire il nome del video';
+$lang['From the video'] = 'Dal video';
+$lang['Logo'] = 'Logo';
+$lang['Please add a thumbnail'] = 'Si prega di aggiungere una miniatura';
+$lang['Color'] = 'Colore';
+$lang['Common configuration'] = 'Configurazione comune';
+$lang['Change'] = 'Modifica';
+$lang['Autoplay'] = 'Autoplay';
+$lang['Author portrait'] = 'Autore ritratto';
+$lang['Author name'] = 'Nome autore';
+$lang['Add film effect'] = 'Aggiungi un\'effetto video';
+$lang['Add a video'] = 'Aggiungi un video';
+?>
