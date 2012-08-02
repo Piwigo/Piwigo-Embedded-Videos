6b86918823b0810e7517b8314e05ed9098409e5e
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-08-02 21:55:46 +0000

    [gvideo] Update pt_PT, thanks to :
    git-svn-id: http://piwigo.org/svn/extensions/gvideo@17336 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/pt_PT/plugin.lang.php
@@ -0,0 +1,43 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Add a video'] = 'Adicionar uma video';
+$lang['Add film effect'] = 'Adicionar um efeito película';
+$lang['Author name'] = 'nome do autor';
+$lang['Author portrait'] = 'Avatar do autor';
+$lang['Autoplay'] = 'Arranque automático';
+$lang['Change'] = 'Mudar';
+$lang['Color'] = 'Cor';
+$lang['Common configuration'] = 'Configuração geral';
+$lang['From the video'] = 'conservar';
+$lang['Logo'] = 'Logotipo';
+$lang['Please add a thumbnail'] = 'Escolhe uma miniatura';
+$lang['Please fill the video URL'] = 'Indique a URL da video';
+$lang['Please fill the video name'] = 'Indicar o nome da foto';
+$lang['Supported services'] = 'Serviços suportados';
+$lang['This element is a video added with "Embedded Video"'] = 'Este elemento é uma video adicionada com "Embedded Video" ';
+$lang['Unable to contact host server'] = 'Impossível contactar o servidor';
+$lang['Use common setting'] = 'Utilizar o parâmetros gerais';
+$lang['Video URL'] = 'URL da video';
+$lang['Video size'] = 'Tamanho da video';
+$lang['Video successfully added. <a href="%s">View</a>'] = 'Video adicionada.<a href="%s">Ver</a>';
+?>
