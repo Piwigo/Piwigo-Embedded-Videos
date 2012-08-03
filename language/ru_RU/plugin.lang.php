cfb40bd63177b53e6ddd226b0838af212c01d59f
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-08-03 18:18:52 +0000

    [gvideo] Update ru_RU, thanks to : rocket
    git-svn-id: http://piwigo.org/svn/extensions/gvideo@17357 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/ru_RU/plugin.lang.php
@@ -0,0 +1,36 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Add a video'] = 'Добавить видео';
+$lang['Add film effect'] = 'Добавить эффект пленки';
+$lang['Author name'] = 'Имя автора';
+$lang['Author portrait'] = 'Изображение автора';
+$lang['Autoplay'] = 'Автопроигрывание';
+$lang['Change'] = 'Изменить';
+$lang['Color'] = 'Цвет';
+$lang['Common configuration'] = 'Общая конфигурация';
+$lang['From the video'] = 'Из видео';
+$lang['Logo'] = 'Лого';
+$lang['Please add a thumbnail'] = 'Пожалуйста, добавьте пиктограмму';
+$lang['Please fill the video URL'] = 'Пожалуйста, заполните видео URL';
+$lang['Please fill the video name'] = 'Пожалуйста, заполните название видео';
+?>
