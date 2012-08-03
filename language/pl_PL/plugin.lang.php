d2f10b139aa2a8b98233d14a7b222ed1db9d9f03
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-08-03 18:19:24 +0000

    [gvideo] Update pl_PL, thanks to : kuba
    git-svn-id: http://piwigo.org/svn/extensions/gvideo@17360 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/pl_PL/plugin.lang.php
@@ -0,0 +1,43 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Common configuration'] = 'Domyślna konfiguracja';
+$lang['From the video'] = 'Z filmu';
+$lang['Supported services'] = 'Wspierane usługi';
+$lang['This element is a video added with "Embedded Video"'] = 'Ten element to film dodany przez "Embedded Video"';
+$lang['Use common setting'] = 'Użyj domyślnych ustawień';
+$lang['Add a video'] = 'Dodaj film';
+$lang['Add film effect'] = 'Dodaj efekt filmowy';
+$lang['Author name'] = 'Nazwisko autora';
+$lang['Author portrait'] = 'Zdjęcie autora';
+$lang['Autoplay'] = 'Auto-odtwarzanie';
+$lang['Change'] = 'Zmień';
+$lang['Color'] = 'Kolor';
+$lang['Logo'] = 'Logo';
+$lang['Please add a thumbnail'] = 'Dodaj miniaturę';
+$lang['Please fill the video URL'] = 'Podaj URL filmu';
+$lang['Please fill the video name'] = 'Podaj nazwę filmu';
+$lang['Unable to contact host server'] = 'Błąd połączenia się z serwerem hosta';
+$lang['Video URL'] = 'URL filmu';
+$lang['Video size'] = 'Rozmiar filmu';
+$lang['Video successfully added. <a href="%s">View</a>'] = 'Film dodany. <a href="%s">Obejrzyj</a>';
+?>
