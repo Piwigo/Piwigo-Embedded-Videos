dce82c11807899146fa47e7e1ae058756ee42c82
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-08-03 18:19:32 +0000

    [gvideo] Update lv_LV, thanks to : agrisans
    git-svn-id: http://piwigo.org/svn/extensions/gvideo@17361 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/lv_LV/plugin.lang.php
@@ -0,0 +1,43 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Video successfully added. <a href="%s">View</a>'] = 'Video sekmīgi pievienots. <a href="%s">Skatīties</a>';
+$lang['Video size'] = 'Video izmērs';
+$lang['Video URL'] = 'Video URL';
+$lang['Use common setting'] = 'Izmantot kopējos uzstādījumus';
+$lang['Unable to contact host server'] = 'Nevar sazināties ar uzņēmējserveri (host)';
+$lang['This element is a video added with "Embedded Video"'] = 'Šis elements ir video, pievienots ar "Iegulto Video"';
+$lang['Supported services'] = 'Atbalstītie pakalpojumi';
+$lang['Please fill the video name'] = 'Lūdzu aizpildiet video nosaukumu';
+$lang['Please fill the video URL'] = 'Lūdzu aizpildiet video URL';
+$lang['Please add a thumbnail'] = 'Lūdzu pievienojiet sīkbildi';
+$lang['Logo'] = 'Logo';
+$lang['From the video'] = 'No video';
+$lang['Common configuration'] = 'Kopējā konfigurācija';
+$lang['Color'] = 'Krāsa';
+$lang['Change'] = 'Mainīt';
+$lang['Autoplay'] = 'Autopalaišana';
+$lang['Author portrait'] = 'Autora portrets';
+$lang['Author name'] = 'Autora vārds';
+$lang['Add film effect'] = 'Pievienot filmas efektu';
+$lang['Add a video'] = 'Pievienot video';
+?>
