6ae576a9ce1db3f62af1be75d9c2c3d76e0aa6b8
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-08-03 18:18:44 +0000

    [gvideo] Update sk_SK, thanks to : dodo
    git-svn-id: http://piwigo.org/svn/extensions/gvideo@17356 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/sk_SK/plugin.lang.php
@@ -0,0 +1,43 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Video successfully added. <a href="%s">View</a>'] = 'Video bolo úspešne pridané. <a href="%s">Zobraziť</a>';
+$lang['Video size'] = 'Veľkosť videa';
+$lang['Video URL'] = 'URL videa';
+$lang['Use common setting'] = 'Použiť všeobecné nastavenie';
+$lang['Unable to contact host server'] = 'Nie je možné sa spojiť s hosťujúcim servrom';
+$lang['This element is a video added with "Embedded Video"'] = 'Tento prvok je video vložené ako "Vložené video" ';
+$lang['Supported services'] = 'Podporované služby';
+$lang['Please fill the video name'] = 'Prosím vyplniť názov videa';
+$lang['Please fill the video URL'] = 'Prosím vyplniť URL videa';
+$lang['Please add a thumbnail'] = 'Prosím pridať náhľad';
+$lang['Logo'] = 'Logo';
+$lang['From the video'] = 'Z videa';
+$lang['Common configuration'] = 'Všeobecná konfigurácia';
+$lang['Color'] = 'Farba';
+$lang['Change'] = 'Zmena';
+$lang['Autoplay'] = 'Automatické prehrávanie';
+$lang['Author portrait'] = 'Portrét autora';
+$lang['Author name'] = 'Meno autora';
+$lang['Add film effect'] = 'Pridať filmový efekt';
+$lang['Add a video'] = 'Pridať video';
+?>
