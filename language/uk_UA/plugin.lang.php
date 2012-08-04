2596f935889fade31cc7dead35f2daa58fe93b35
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-08-04 02:52:45 +0000

    [gvideo] Update uk_UA, thanks to : animan
    git-svn-id: http://piwigo.org/svn/extensions/gvideo@17377 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/uk_UA/plugin.lang.php
@@ -0,0 +1,43 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Add a video'] = 'Додати відео';
+$lang['Add film effect'] = 'Додайте ефект фільму';
+$lang['Author name'] = 'Ім\'я автора';
+$lang['Author portrait'] = 'Автор портрет';
+$lang['Autoplay'] = 'Автовідтворення';
+$lang['Change'] = 'Змінити';
+$lang['Color'] = 'Колір';
+$lang['Common configuration'] = 'Загальна конфігурація';
+$lang['From the video'] = 'Від відео';
+$lang['Logo'] = 'Логотип';
+$lang['Please add a thumbnail'] = 'Будь ласка, додайте мініатюру';
+$lang['Please fill the video URL'] = 'Будь ласка, введіть URL відео';
+$lang['Please fill the video name'] = 'Будь ласка, заповніть ім\'я відео';
+$lang['Supported services'] = 'Підтримувані послуги';
+$lang['This element is a video added with "Embedded Video"'] = 'Цей елемент додано з "Embedded Video"';
+$lang['Unable to contact host server'] = 'Не вдалося підключитись до вузла сервера';
+$lang['Use common setting'] = 'Використовувати загальні налаштування';
+$lang['Video URL'] = 'URL відео ';
+$lang['Video size'] = 'Розмір відео';
+$lang['Video successfully added. <a href="%s">View</a>'] = 'Відео успішно додано. <a href="%s">Переглянути</a>';
+?>
