63f66e940760d1b9ee520ef1b82b895cc5112cd4
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-08-04 14:33:37 +0000

    [gvideo] Update cs_CZ, thanks to : sichr
    git-svn-id: http://piwigo.org/svn/extensions/gvideo@17390 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/cs_CZ/plugin.lang.php
@@ -0,0 +1,39 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Add a video'] = 'Přidat video';
+$lang['Add film effect'] = 'Přidat filmový efekt';
+$lang['Author name'] = 'Jméno autora';
+$lang['Author portrait'] = 'Autorův portrét';
+$lang['Autoplay'] = 'Automatické přehrávání';
+$lang['Color'] = 'Barva';
+$lang['Common configuration'] = 'Běžné nastavení';
+$lang['Logo'] = 'Logo';
+$lang['Please fill the video URL'] = 'Vyplňte prosím URL videa';
+$lang['Supported services'] = 'Podporované služby';
+$lang['This element is a video added with "Embedded Video"'] = 'Tento element je video vložené pomocí "Embedded video"';
+$lang['Unable to contact host server'] = 'Nepodařilo se navázat spojení s hostitelským serverem';
+$lang['Use common setting'] = 'Použít běžné nastavení';
+$lang['Video URL'] = 'URL videa';
+$lang['Video size'] = 'Velikost videa';
+$lang['Video successfully added. <a href="%s">View</a>'] = 'Video úspěšně vloženo. <a href="%s">Zobrazit</a> ';
+?>
