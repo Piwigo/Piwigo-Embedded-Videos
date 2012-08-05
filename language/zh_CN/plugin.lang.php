3e6bbacd77652967e974561896aadaa39e413d25
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-08-05 16:31:15 +0000

    [gvideo] Update zh_CN, thanks to : jimhu
    git-svn-id: http://piwigo.org/svn/extensions/gvideo@17410 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/zh_CN/plugin.lang.php
@@ -0,0 +1,40 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Add a video'] = '添加一个视频';
+$lang['Add film effect'] = '添加电影效果';
+$lang['Author name'] = '作者姓名';
+$lang['Author portrait'] = '作者肖像';
+$lang['Autoplay'] = '自动播放';
+$lang['Change'] = '变更';
+$lang['Color'] = '颜色';
+$lang['Common configuration'] = '常规设置';
+$lang['Logo'] = '商标';
+$lang['Please fill the video URL'] = '请填入视频 URL';
+$lang['Supported services'] = '支持的服务';
+$lang['This element is a video added with "Embedded Video"'] = '该组件用于添加“嵌入视频”';
+$lang['Unable to contact host server'] = '不能连接到服务器';
+$lang['Use common setting'] = '使用常规设置';
+$lang['Video URL'] = '视频 URL';
+$lang['Video size'] = '视频大小';
+$lang['Video successfully added. <a href="%s">View</a>'] = '成功添加视频。<a href="%s">查看</a>';
+?>
