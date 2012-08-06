707ac05d68c5d1f6c87486ead1d9421ce3265fe5
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-08-06 10:34:57 +0000

    [gvideo] Update nl_NL, thanks to : EdwinKort
    git-svn-id: http://piwigo.org/svn/extensions/gvideo@17431 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/nl_NL/plugin.lang.php
@@ -0,0 +1,40 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Supported services'] = 'Ondersteunde apparaten';
+$lang['Add a video'] = 'Voeg een video toe';
+$lang['Add film effect'] = 'Voeg een film effect toe';
+$lang['Author name'] = 'Auteur naam';
+$lang['Author portrait'] = 'Auteur foto';
+$lang['Autoplay'] = 'Autoplay';
+$lang['Change'] = 'Verander';
+$lang['Color'] = 'Kleur';
+$lang['Common configuration'] = 'Algemene configuratie';
+$lang['Logo'] = 'Logo';
+$lang['Please fill the video URL'] = 'Vul aub de url van de video in';
+$lang['This element is a video added with "Embedded Video"'] = 'Dit element is een video toegevoegd met \'Embedded video\'';
+$lang['Unable to contact host server'] = 'Verbinding met host server niet mogelijk';
+$lang['Use common setting'] = 'Gebruik algemene instelling';
+$lang['Video URL'] = 'Video URL';
+$lang['Video size'] = 'Video formaat';
+$lang['Video successfully added. <a href="%s">View</a>'] = 'Video succesvol toegevoegd. <a href="%s">Bekijk</a>';
+?>
