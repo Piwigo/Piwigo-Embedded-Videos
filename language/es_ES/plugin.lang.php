1f3379f705ed1cb668f7afd3e62c670fdb6523cf
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-08-07 16:23:44 +0000

    [gvideo] Update es_ES, thanks to : petaqui
    git-svn-id: http://piwigo.org/svn/extensions/gvideo@17467 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/es_ES/plugin.lang.php
@@ -0,0 +1,40 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Add a video'] = 'Añadir vídeo';
+$lang['Add film effect'] = 'Añadir efecto de vídeo';
+$lang['Author name'] = 'Nombre del autor';
+$lang['Author portrait'] = 'Foto del autor';
+$lang['Autoplay'] = 'Autoreproducción';
+$lang['Change'] = 'Cambiar';
+$lang['Color'] = 'Color';
+$lang['Common configuration'] = 'Configuración común';
+$lang['Logo'] = 'Logo';
+$lang['Please fill the video URL'] = 'Por favor ponga la url del vídeo';
+$lang['Supported services'] = 'Servicios soportados';
+$lang['This element is a video added with "Embedded Video"'] = 'Este elemento es un vídeo añadido con "Embedded Video"';
+$lang['Unable to contact host server'] = 'Imposible contactar con el servidor';
+$lang['Use common setting'] = 'Usar ajustes comunes';
+$lang['Video URL'] = 'URL del vídeo';
+$lang['Video size'] = 'Tamaño del vídeo';
+$lang['Video successfully added. <a href="%s">View</a>'] = 'Vídeo correctamente añadido. <a href="%s">Ver</a>';
+?>
