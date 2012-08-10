00415d1d37b19447ccdd3d5f6650649593c46895
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-08-10 12:13:48 +0000

    [gvideo] Update tr_TR, thanks to : LazBoy
    git-svn-id: http://piwigo.org/svn/extensions/gvideo@17537 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/tr_TR/plugin.lang.php
@@ -0,0 +1,40 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['This element is a video added with "Embedded Video"'] = 'Bu öğe "Embedded Video" ile eklenen bir videodur.';
+$lang['Video successfully added. <a href="%s">View</a>'] = 'Video başarıyla eklendi. <a href="%s">Gözat</a>';
+$lang['Add a video'] = 'Bir video ekle';
+$lang['Add film effect'] = 'Film efekti ekle';
+$lang['Author name'] = 'Yazar adı';
+$lang['Author portrait'] = 'Yazar portresi';
+$lang['Autoplay'] = 'Otomatik oynat';
+$lang['Change'] = 'Değiştir';
+$lang['Color'] = 'Renk';
+$lang['Common configuration'] = 'Genel yapılandırma';
+$lang['Logo'] = 'Logo';
+$lang['Please fill the video URL'] = 'Lütfen video URL\'sini girin';
+$lang['Supported services'] = 'Desteklenen servisler';
+$lang['Unable to contact host server'] = 'Sunucu ile bağlantı kurulamadı';
+$lang['Use common setting'] = 'Genel ayarları kullan';
+$lang['Video URL'] = 'Video URL';
+$lang['Video size'] = 'Video büyüklüğü';
+?>
