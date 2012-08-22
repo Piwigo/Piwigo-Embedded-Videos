085d00ee118107480e815fc4391f2d3083e7a8e2
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-08-22 16:51:24 +0000

    [gvideo] Update pt_BR, thanks to : oigreslima
    git-svn-id: http://piwigo.org/svn/extensions/gvideo@17624 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/pt_BR/plugin.lang.php
@@ -0,0 +1,40 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Use common setting'] = 'Use a configuração Padrão';
+$lang['Add a video'] = 'Adicione um Vídeo';
+$lang['Add film effect'] = 'Adicione um efeito ';
+$lang['Author name'] = 'Nome do Autor';
+$lang['Author portrait'] = 'Retrato do Autor';
+$lang['Autoplay'] = 'Tocar automático';
+$lang['Change'] = 'Mudar';
+$lang['Color'] = 'Cor';
+$lang['Common configuration'] = 'Configuração padrão';
+$lang['Logo'] = 'Logo';
+$lang['Please fill the video URL'] = 'Por favor preencha a URL do Vídeo';
+$lang['Supported services'] = 'Serviços suportados';
+$lang['This element is a video added with "Embedded Video"'] = 'Esse elemento é um vídeo adicionado com "Embedded Vídeo"';
+$lang['Unable to contact host server'] = 'Não foi possível acessar o servidor';
+$lang['Video URL'] = 'URL do Vídeo';
+$lang['Video size'] = 'Tamanho do Vídeo';
+$lang['Video successfully added. <a href="%s">View</a>'] = 'Vídeo adicionado com sucesso. <a href="%s">Ver</a>';
+?>
