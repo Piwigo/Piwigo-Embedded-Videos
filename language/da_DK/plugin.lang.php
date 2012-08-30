2b8ef69c49dc0b24d2a72845d379e79a04bacaf8
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-08-30 18:44:22 +0000

    [gvideo] Update da_DK, thanks to : Egorip
    git-svn-id: http://piwigo.org/svn/extensions/gvideo@17673 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/da_DK/plugin.lang.php
@@ -0,0 +1,44 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Video successfully updated'] = 'Videoen blevet opdateret';
+$lang['Add a video'] = 'Tilføj en video';
+$lang['Add film effect'] = 'Tilføj film effekt';
+$lang['Author name'] = 'Forfatter navn';
+$lang['Author portrait'] = 'Forfatter portræt';
+$lang['Autoplay'] = 'Automatisk afspilning';
+$lang['Change'] = 'Ændre';
+$lang['Changing the url will reset video description, name and thumbnail'] = 'Ændring af url vil nulstille video beskrivelse, navn og miniaturebilledet';
+$lang['Color'] = 'Farve';
+$lang['Common configuration'] = 'Fælles konfiguration';
+$lang['Logo'] = 'Logo';
+$lang['Please fill the video URL'] = 'Udfyld venligst video URL\'en';
+$lang['Supported services'] = 'Understøttede tjenester';
+$lang['The thumbnail was updated'] = 'Miniaturebilledet blev opdateret';
+$lang['This element is a video added with "Embedded Video"'] = 'Dette element er en video tilføjet med "Embedded Video"';
+$lang['Unable to contact host server'] = 'Kan ikke kontakte værtsserver';
+$lang['Upload a new thumbnail'] = 'Upload et nyt miniaturebillede';
+$lang['Use common setting'] = 'Brug fælles indstilling';
+$lang['Video URL'] = 'Video URL';
+$lang['Video size'] = 'Video størrelse';
+$lang['Video successfully added. <a href="%s">View</a>'] = 'Videoen blevet tilføjet. <a href="%s">Vis</a>';
+?>
