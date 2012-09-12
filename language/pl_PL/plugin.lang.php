3b5110d9bf889cc640a9821b1c54a9254178eb6b
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-09-12 18:43:17 +0000

    [gvideo] Update pl_PL, thanks to : larky
    git-svn-id: http://piwigo.org/svn/extensions/gvideo@17875 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/pl_PL/plugin.lang.php
+++ language/pl_PL/plugin.lang.php
@@ -21,7 +21,6 @@
 // | USA.                                                                  |
 // +-----------------------------------------------------------------------+
 $lang['Common configuration'] = 'Domyślna konfiguracja';
-$lang['From the video'] = 'Z filmu';
 $lang['Supported services'] = 'Wspierane usługi';
 $lang['This element is a video added with "Embedded Video"'] = 'Ten element to film dodany przez "Embedded Video"';
 $lang['Use common setting'] = 'Użyj domyślnych ustawień';
@@ -33,11 +32,13 @@ $lang['Autoplay'] = 'Auto-odtwarzanie';
 $lang['Change'] = 'Zmień';
 $lang['Color'] = 'Kolor';
 $lang['Logo'] = 'Logo';
-$lang['Please add a thumbnail'] = 'Dodaj miniaturę';
 $lang['Please fill the video URL'] = 'Podaj URL filmu';
-$lang['Please fill the video name'] = 'Podaj nazwę filmu';
 $lang['Unable to contact host server'] = 'Błąd połączenia się z serwerem hosta';
 $lang['Video URL'] = 'URL filmu';
 $lang['Video size'] = 'Rozmiar filmu';
 $lang['Video successfully added. <a href="%s">View</a>'] = 'Film dodany. <a href="%s">Obejrzyj</a>';
+$lang['Changing the url will reset video description, name and thumbnail'] = 'Zmiana adresu URL zresetuje opis video, nazwę i miniaturę';
+$lang['The thumbnail was updated'] = 'Miniatura została uaktualniona';
+$lang['Upload a new thumbnail'] = 'Wgraj nową miniaturę';
+$lang['Video successfully updated'] = 'Aktualizacja video zakończona sukcesem';
 ?>
