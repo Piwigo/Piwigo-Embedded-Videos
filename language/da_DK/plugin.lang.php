134952b8d2dbe3f4640f5b658cdb7e5178991a7b
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-09-18 18:56:43 +0000

    [gvideo] Update da_DK, thanks to : Kaare
    git-svn-id: http://piwigo.org/svn/extensions/gvideo@17997 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/da_DK/plugin.lang.php
+++ language/da_DK/plugin.lang.php
@@ -22,23 +22,23 @@
 // +-----------------------------------------------------------------------+
 $lang['Video successfully updated'] = 'Videoen blevet opdateret';
 $lang['Add a video'] = 'Tilføj en video';
-$lang['Add film effect'] = 'Tilføj film effekt';
+$lang['Add film effect'] = 'Tilføj filmeffekt';
 $lang['Author name'] = 'Forfatter navn';
-$lang['Author portrait'] = 'Forfatter portræt';
+$lang['Author portrait'] = 'Forfatters portræt';
 $lang['Autoplay'] = 'Automatisk afspilning';
-$lang['Change'] = 'Ændre';
-$lang['Changing the url will reset video description, name and thumbnail'] = 'Ændring af url vil nulstille video beskrivelse, navn og miniaturebilledet';
+$lang['Change'] = 'Ændr';
+$lang['Changing the url will reset video description, name and thumbnail'] = 'Ændring af url vil nulstille videobeskrivelse, navn og miniaturebilledet';
 $lang['Color'] = 'Farve';
-$lang['Common configuration'] = 'Fælles konfiguration';
+$lang['Common configuration'] = 'Fælles opsætning';
 $lang['Logo'] = 'Logo';
-$lang['Please fill the video URL'] = 'Udfyld venligst video URL\'en';
+$lang['Please fill the video URL'] = 'Angiv videoens URL';
 $lang['Supported services'] = 'Understøttede tjenester';
 $lang['The thumbnail was updated'] = 'Miniaturebilledet blev opdateret';
 $lang['This element is a video added with "Embedded Video"'] = 'Dette element er en video tilføjet med "Embedded Video"';
 $lang['Unable to contact host server'] = 'Kan ikke kontakte værtsserver';
 $lang['Upload a new thumbnail'] = 'Upload et nyt miniaturebillede';
-$lang['Use common setting'] = 'Brug fælles indstilling';
-$lang['Video URL'] = 'Video URL';
-$lang['Video size'] = 'Video størrelse';
-$lang['Video successfully added. <a href="%s">View</a>'] = 'Videoen blevet tilføjet. <a href="%s">Vis</a>';
+$lang['Use common setting'] = 'Brug fælles opsætning';
+$lang['Video URL'] = 'Video-URL';
+$lang['Video size'] = 'Videostørrelse';
+$lang['Video successfully added. <a href="%s">View</a>'] = 'Videoen blevet tilføjet. <a href="%s">Se</a>';
 ?>
