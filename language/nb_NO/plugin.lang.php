b3dfd9f3977d6760a33a417182606626c124c06d
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-10-06 13:05:33 +0000

    [gvideo] Update nb_NO, thanks to : abbamust
    git-svn-id: http://piwigo.org/svn/extensions/gvideo@18518 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/nb_NO/plugin.lang.php
@@ -0,0 +1,44 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Video successfully updated'] = 'Video oppdatert';
+$lang['Add a video'] = 'Legg til en video';
+$lang['Add film effect'] = 'Legg til fileffekt';
+$lang['Author name'] = 'Eiers navn';
+$lang['Author portrait'] = 'Eiers portrett';
+$lang['Autoplay'] = 'Spill automatisk';
+$lang['Change'] = 'Bytt';
+$lang['Changing the url will reset video description, name and thumbnail'] = 'Ved bytte av URL vil video beskrivelse, navn og miniatyr bli nullstilt';
+$lang['Color'] = 'Farge';
+$lang['Common configuration'] = 'Vanlig konfigurasjon';
+$lang['Logo'] = 'Logo';
+$lang['Please fill the video URL'] = 'Vennligst fyll inn videoens URL';
+$lang['Supported services'] = 'Støttede tjenester';
+$lang['The thumbnail was updated'] = 'Miniatyrbilde ble oppdatert';
+$lang['This element is a video added with "Embedded Video"'] = 'Dette elementet er en video lagt til med "Embedded Video"';
+$lang['Unable to contact host server'] = 'Kunne ikke kontakte serveren';
+$lang['Upload a new thumbnail'] = 'Last opp ny miniatyr';
+$lang['Use common setting'] = 'Bruk vanlig konfigurasjon';
+$lang['Video URL'] = 'Video URL';
+$lang['Video size'] = 'Video størrelse';
+$lang['Video successfully added. <a href="%s">View</a>'] = 'Video lagt til. <a href="%s">Se</a>';
+?>
