ec56cecc6ce8ca3c9c1cdf78f972132ce1ae9948
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-11-23 05:33:10 +0000

    [gvideo] Update bg_BG, thanks to : picsvet
    git-svn-id: http://piwigo.org/svn/extensions/gvideo@19139 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/bg_BG/plugin.lang.php
@@ -0,0 +1,49 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Video URL'] = 'Видео URL';
+$lang['Video size'] = 'Видео размер';
+$lang['Video successfully added. <a href="%s">View</a>'] = 'Видеото е добавено успешно. <a href="%s">
+Преглед</a>';
+$lang['Video successfully updated'] = 'Видеото е актуализирано успешно';
+$lang['Vimeo player'] = 'Vimeo плейър';
+$lang['Add a video'] = 'Добави видео';
+$lang['Add film effect'] = 'Добави филмов ефект';
+$lang['Author name'] = 'Име на автора';
+$lang['Author portrait'] = 'Портрет на автора';
+$lang['Autoplay'] = 'Автостарт';
+$lang['Change'] = 'Промяна';
+$lang['Changing the url will reset video description, name and thumbnail'] = 'Промяна на URL ще нулира описанието,името и малката снимка на видеото ';
+$lang['Color'] = 'Цвят';
+$lang['Common configuration'] = 'Общата конфигурация';
+$lang['Dailymotion player'] = 'Dailymotion плейър';
+$lang['Get video description'] = 'Вземи видео описание';
+$lang['Get video tags'] = 'Вземи видео тагове';
+$lang['Logo'] = 'Лого';
+$lang['Please fill the video URL'] = 'Моля,въведете видео URL';
+$lang['Supported services'] = 'Поддържани услуги';
+$lang['The thumbnail was updated'] = 'Миниизображението беше актуализирано';
+$lang['This element is a video added with "Embedded Video"'] = 'Този елемент е видео добавено със "Embedded Video"';
+$lang['Unable to contact host server'] = 'Не може да се свържете с хост сървъра';
+$lang['Upload a new thumbnail'] = 'Качване на нова умалена снимка';
+$lang['Use common setting'] = 'Използвайте настройката';
+?>
