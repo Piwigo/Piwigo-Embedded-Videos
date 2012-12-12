ff854f5a0c53f44be21a65e8225ed074d9cc415b
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-12-12 14:14:36 +0000

    [gvideo] Update ca_ES, thanks to : josep.torne
    git-svn-id: http://piwigo.org/svn/extensions/gvideo@19401 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/ca_ES/plugin.lang.php
@@ -0,0 +1,52 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Unable to contact host server'] = 'No s\'ha pogut contactar amb el servidor';
+$lang['Upload a new thumbnail'] = 'Penja una nova miniatura';
+$lang['Use common setting'] = 'Utilitza la configuració comuna';
+$lang['Video URL'] = 'URL del vídeo';
+$lang['Video properties'] = 'Propietats del vídeo';
+$lang['Video size'] = 'Mida del vídeo';
+$lang['Video successfully added. <a href="%s">View</a>'] = 'El vídeo s\'ha afegit correctament. <a href="%s">Veure</a>';
+$lang['Video successfully updated'] = 'El video s\'ha afegit correctament';
+$lang['Vimeo player'] = 'Reproductor de Vimeo';
+$lang['Autoplay'] = 'Autoplay';
+$lang['Add a video'] = 'Afegeix un vídeo';
+$lang['Add film effect'] = 'Afegeix un efecte de vídeo';
+$lang['Author name'] = 'Nom de l\'autor';
+$lang['Author portrait'] = 'Retrat de l\'autor';
+$lang['Change'] = 'Canvia';
+$lang['Changing the url will reset video description, name and thumbnail'] = 's\'està canviant la URL. Es reiniciarà la descripció, el nom i la miniatura';
+$lang['Color'] = 'Color';
+$lang['Common configuration'] = 'configuració comuna';
+$lang['Dailymotion player'] = 'Reproductor Dailymotion';
+$lang['Get video description'] = 'Descripció de Getvideo';
+$lang['Get video tags'] = 'Etiquetes de Getvideo';
+$lang['In safe-mode, the plugin does\'t try to contact the video host, usefull on some websites where remote connections are blocked.'] = 'En mode segur, el connector no intenta contactar amb l\'ordinador del vídeo, útil en alguns llocs webs on les connexions remotes estan bloquejades. ';
+$lang['Logo'] = 'Logotip';
+$lang['Please fill the video URL'] = 'Si us plau, ompliu la URL del vídeo';
+$lang['Safe-mode'] = 'Mode segur';
+$lang['Supported services'] = 'Serveis soportats';
+$lang['The thumbnail was updated'] = 'S\'ha actualitzat la miniatura';
+$lang['This element is a video added with "Embedded Video"'] = 'Aquest element és un vídeo afegit amb "Embedded Video"';
+$lang['Try in safe-mode'] = 'Intenta-ho en mode segur';
+?>
