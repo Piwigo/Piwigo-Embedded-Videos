279b211481901ac69cdafb787c7ebcbad1092624
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-02-13 21:07:14 +0000

    [gvideo] Update fa_IR, thanks to : Garcia
    git-svn-id: http://piwigo.org/svn/extensions/gvideo@20751 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/fa_IR/plugin.lang.php
@@ -0,0 +1,52 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Add a video'] = 'افزودن یک ویدیو';
+$lang['Add film effect'] = 'افزودن جلوه‌ی فیلم';
+$lang['Author name'] = 'نام سازنده';
+$lang['Author portrait'] = 'عکس سازنده';
+$lang['Autoplay'] = 'پخش خودکار';
+$lang['Change'] = 'تغییر';
+$lang['Changing the url will reset video description, name and thumbnail'] = 'تغییر نشانی فیلم، توضیحات، نام و عکس ریزه‌ی آنرا بازنشانی خواهد کرد';
+$lang['Color'] = 'رنگ';
+$lang['Common configuration'] = 'پیکربندی فراگیر';
+$lang['Dailymotion player'] = 'پخش کننده‌ی Dailymotion';
+$lang['Get video description'] = 'دریافت توضیحات ویدیو';
+$lang['Get video tags'] = 'دریافت برچسب‌های ویدیو';
+$lang['In safe-mode, the plugin does\'t try to contact the video host, usefull on some websites where remote connections are blocked.'] = 'در حالت امن (Safe-Mode)، افزونه به میزبان فیلم تماس برقرار نمی‌کند، کارایی این حالت برای وبسایت‌هایی که ارتباط از راه دور (Remote Connection) با آنها بسته است.';
+$lang['Logo'] = 'لوگو';
+$lang['Please fill the video URL'] = 'نشانی ویدیو را بنویسید';
+$lang['Safe-mode'] = 'حالت امن';
+$lang['Supported services'] = 'سرویس‌های پشتیبانی شده';
+$lang['The thumbnail was updated'] = 'عکس ریزه به روز شد';
+$lang['This element is a video added with "Embedded Video"'] = 'این عنصر ویدیوییست که با «ویدیوی جاسازی شده» افزوده شده است';
+$lang['Try in safe-mode'] = 'در حالت امن امتحان کنید';
+$lang['Unable to contact host server'] = 'نمی‌توان با میزبان تماس گرفت';
+$lang['Upload a new thumbnail'] = 'آپلود یک عکس ریزه‌ی تازه';
+$lang['Use common setting'] = 'استفاده از تنظیمات فراگیر';
+$lang['Video URL'] = 'نشانی ویدیو';
+$lang['Video properties'] = 'ویژگی‌های ویدیو';
+$lang['Video size'] = 'سایز ویدیو';
+$lang['Video successfully added. <a href="%s">View</a>'] = 'ویدیو به درستی افزوده شد. <a href="%s">نمایش</a>';
+$lang['Video successfully updated'] = 'ویدیو به درستی آپلود شد';
+$lang['Vimeo player'] = 'پخش کننده‌ی Vimeo';
+?>
