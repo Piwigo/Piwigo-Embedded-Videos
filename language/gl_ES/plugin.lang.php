d9628914cffb29e7904a12ec70290474796a94dd
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-03-20 12:41:12 +0000

    [gvideo] Add gl_ES, thanks to : damufo
    git-svn-id: http://piwigo.org/svn/extensions/gvideo@21739 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/gl_ES/plugin.lang.php
@@ -0,0 +1,57 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['This element is a video added with "Embedded Video"'] = 'Este elemento é un vídeo engadido con "Vídeo embebido"';
+$lang['Videos can be unlisted and private if the gallery website is within the authorized domains (PRO).'] = 'Os vídeos pode ser non listados e privados se a galería do sitio web está dentro dos dominios autorizados (PRO).';
+$lang['Videos can be unlisted but not private.'] = 'Os vídeos poden ser non listados pero non privados.';
+$lang['Try in safe-mode'] = 'Intentar co modo seguro';
+$lang['Unable to contact host server'] = 'Non se puido contactar co servidor';
+$lang['Upload a new thumbnail'] = 'Enviar unha nova miniatura';
+$lang['Use common setting'] = 'Empregar a configuración predeterminada';
+$lang['Video URL'] = 'URL do vídeo';
+$lang['Video properties'] = 'Propiedades do vídeo';
+$lang['Video size'] = 'Tamaño do vídeo';
+$lang['Video successfully added. <a href="%s">View</a>'] = 'O vídeo engadiuse con éxito. <a href="%s">Ver</a>';
+$lang['Video successfully updated'] = 'O vídeo actualizouse con éxito';
+$lang['Videos can be private if you use the private permalink.'] = 'Os vídeos poden ser privados se empregas a ligazón permanente privada.';
+$lang['Vimeo player'] = 'Reprodutor Vimeo';
+$lang['Dailymotion player'] = 'Reprodutor Dailymotion';
+$lang['The thumbnail was updated'] = 'Actualizouse a miniatura do vídeo';
+$lang['Changing the url will reset video description, name and thumbnail'] = 'Mudando o URL borrarase a descrición, o nome e a icona do vídeo.';
+$lang['Common configuration'] = 'Configuración predeterminada';
+$lang['Autoplay'] = 'Reprodución automática';
+$lang['Change'] = 'Mudar';
+$lang['Color'] = 'Cor';
+$lang['Get video description'] = 'Obter a descrición do vídeo';
+$lang['Get video tags'] = 'Obter as etiquetas do vídeo';
+$lang['In safe-mode, the plugin does\'t try to contact the video host, usefull on some websites where remote connections are blocked.'] = 'No modo seguro, o engadido non intenta poñerse en contacto co servidor do vídeo, útil nalgúns sitios onde se bloquean as conexións remotas.';
+$lang['Logo'] = 'Logotipo';
+$lang['No privacy option.'] = 'Opción sen privacidade.';
+$lang['Please fill the video URL'] = 'Insira o URL do vídeo';
+$lang['Safe-mode'] = 'Modo seguro';
+$lang['Show privacy details'] = 'Amosar os detalles da privacidade';
+$lang['Supported services'] = 'Servizos compatibles';
+$lang['Add film effect'] = 'Engadir un efecto de vídeo';
+$lang['Author name'] = 'Nome do autor';
+$lang['Author portrait'] = 'Retrato do autor';
+$lang['Add a video'] = 'Engadir un vídeo';
+?>
