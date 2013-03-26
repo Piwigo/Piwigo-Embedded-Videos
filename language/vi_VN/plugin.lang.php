c1f3314ca2e5603941103545ecdc91c3f9c650d1
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-03-26 18:27:18 +0000

    [gvideo] Update vi_VN, thanks to : icy
    git-svn-id: http://piwigo.org/svn/extensions/gvideo@21850 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/vi_VN/plugin.lang.php
@@ -0,0 +1,56 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['This element is a video added with "Embedded Video"'] = 'Video này là để nhúng vào các bài viết';
+$lang['Try in safe-mode'] = 'Thử với safe-mode (chế độ an toàn)';
+$lang['Unable to contact host server'] = 'Không thể kết nối tới máy lưu trữ video';
+$lang['Upload a new thumbnail'] = 'Tải lên hình đại diện mới';
+$lang['Use common setting'] = 'Dùng thiết lập chung';
+$lang['Video URL'] = 'Địa chỉ video';
+$lang['Video properties'] = 'Đặc điểm của video';
+$lang['Video size'] = 'Kích cỡ video';
+$lang['Video successfully added. <a href="%s">View</a>'] = 'Đã thêm video xong. <a href="%s">Xem ở đây</a>';
+$lang['Video successfully updated'] = 'Cập nhật thông tin xong';
+$lang['Videos can be private if you use the private permalink.'] = 'Dùng các địa chỉ bí mật để bảo vệ video';
+$lang['Videos can be unlisted and private if the gallery website is within the authorized domains (PRO).'] = 'Video có thể không được liệt kê và đặt vào chế độ riêng tư nếu địa chỉ trang web có liên quan tới PRO (các tên miền có xác nhận quyền sử dụng riêng)';
+$lang['Videos can be unlisted but not private.'] = 'Video có thể không được liệt kê nhưng không ở chế độ riêng tư';
+$lang['Vimeo player'] = 'Trình xem Vimeo';
+$lang['Add a video'] = 'Thêm video';
+$lang['Add film effect'] = 'Thêm hiệu ứng cho phim';
+$lang['Author name'] = 'Tên người dựng phim';
+$lang['Author portrait'] = 'Ảnh tác giả';
+$lang['Autoplay'] = 'Tự chạy';
+$lang['Change'] = 'Thay đổi';
+$lang['Changing the url will reset video description, name and thumbnail'] = 'Đổi địa chỉ sẽ xóa đi thông tin mô tả, tên và bản demo của video';
+$lang['Color'] = 'Màu';
+$lang['Common configuration'] = 'Thiết lập chung';
+$lang['Get video description'] = 'Lấy mô tả của video';
+$lang['Get video tags'] = 'Lấy thẻ mô tả video';
+$lang['In safe-mode, the plugin does\'t try to contact the video host, usefull on some websites where remote connections are blocked.'] = 'Ở safe-mode sẽ không có tuơng tác với máy lưu trữ video (có ích khi một số máy chủ chặn kết nối)';
+$lang['Logo'] = 'Logo';
+$lang['No privacy option.'] = 'Không có chính sách bảo vệ';
+$lang['Please fill the video URL'] = 'Hãy cho biết địa chỉ video';
+$lang['Safe-mode'] = 'Safe-mode (chế độ an toàn)';
+$lang['Show privacy details'] = 'Chi tiết về chính sách bảo vệ';
+$lang['Supported services'] = 'Dịch vụ có hỗ trợ';
+$lang['The thumbnail was updated'] = 'Đã cập nhật bản demo';
+?>
