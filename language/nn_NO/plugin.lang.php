4693640978de0a7f5a68beccf7308b6592c0620d
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-03-28 20:38:46 +0000

    [gvideo] Add nn_NO, thanks to : kiho
    git-svn-id: http://piwigo.org/svn/extensions/gvideo@21884 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/nn_NO/plugin.lang.php
@@ -0,0 +1,57 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['This element is a video added with "Embedded Video"'] = 'Dette elementet er ein video lagt til med tilleggsprogrammet "Embedded Video"';
+$lang['Try in safe-mode'] = 'Prøv i sikkermodus';
+$lang['Unable to contact host server'] = 'Greier ikkje å få kontakt med videoserver';
+$lang['Upload a new thumbnail'] = 'Last opp eit nytt miniatyrbilete';
+$lang['Use common setting'] = 'Bruk felles innstillingar';
+$lang['Video URL'] = 'URL til video';
+$lang['Video properties'] = 'Eigenskapar til video';
+$lang['Video size'] = 'Videostørrelse';
+$lang['Video successfully added. <a href="%s">View</a>'] = 'Video vart lagt til <a href="%s">Vis</a>';
+$lang['Video successfully updated'] = 'Video vart oppdatera';
+$lang['Videos can be private if you use the private permalink.'] = 'Videoar kan vera private om du bruker private permalenker.';
+$lang['Videos can be unlisted and private if the gallery website is within the authorized domains (PRO).'] = 'Videoen kan fjernas frå lister og gjerast privat om nettstaden til galleriet er innan godkjende domene (PRO).';
+$lang['Videos can be unlisted but not private.'] = 'Videoen kan fjernast frå lister, men ikkje gjerast privat.';
+$lang['Vimeo player'] = 'Vimeo spelar';
+$lang['Add a video'] = 'Legg til video';
+$lang['Add film effect'] = 'Legg til filmeffekt';
+$lang['Author name'] = 'Forfattarnamn';
+$lang['Author portrait'] = 'Forfattarbilete';
+$lang['Autoplay'] = 'Start automatisk';
+$lang['Change'] = 'Endre';
+$lang['Changing the url will reset video description, name and thumbnail'] = 'Endring av URL vil nullstille opplysningar, navn og miniatyrbilete for videoen';
+$lang['Color'] = 'Farge';
+$lang['Common configuration'] = 'Felles konfigurering';
+$lang['Dailymotion player'] = 'Daylymotion spelar';
+$lang['Get video description'] = 'Hent video opplysningar';
+$lang['Get video tags'] = 'Hent stikkord';
+$lang['In safe-mode, the plugin does\'t try to contact the video host, usefull on some websites where remote connections are blocked.'] = 'I sikkermodus, vil ikkje tilleggsprogrammet ta kontakt med videoserveren - dette er nyttig på nettstader der tilkoplingar til andre serverar er blokkera.';
+$lang['Logo'] = 'Logo';
+$lang['No privacy option.'] = 'Ingen personverninnstillingar';
+$lang['Please fill the video URL'] = 'Skriv inn URL til video';
+$lang['Safe-mode'] = 'Sikkermodus';
+$lang['Show privacy details'] = 'Vis personverninnstillingar';
+$lang['Supported services'] = 'Støtta tenestar';
+$lang['The thumbnail was updated'] = 'Miniatyrbilete er oppdatera';
+?>
