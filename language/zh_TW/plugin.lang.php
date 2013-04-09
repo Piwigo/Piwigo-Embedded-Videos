9d28f3cab2e8ae5d32fb406b3a386c35a9508891
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-04-09 12:14:54 +0000

    [gvideo] Update zh_TW, thanks to : tedx
    git-svn-id: http://piwigo.org/svn/extensions/gvideo@22079 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/zh_TW/plugin.lang.php
@@ -0,0 +1,57 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Video properties'] = '影片屬性';
+$lang['Video size'] = '影片大小';
+$lang['Video successfully added. <a href="%s">View</a>'] = '影片成功加入<a href="%s">瀏覽</a>';
+$lang['Video successfully updated'] = '影片更新成功';
+$lang['Videos can be private if you use the private permalink.'] = '影片也可以是私人，如果你使用的是私人永久連結。';
+$lang['Videos can be unlisted and private if the gallery website is within the authorized domains (PRO).'] = '影片可以不公開及私人畫廊網站是在授權網域（PRO）。';
+$lang['Videos can be unlisted but not private.'] = '影片可以不公開，但不是私有。';
+$lang['Vimeo player'] = 'Vimeo 播放器';
+$lang['Please fill the video URL'] = '請填寫影片網址';
+$lang['Add a video'] = '新增影片';
+$lang['Changing the url will reset video description, name and thumbnail'] = '更改URL會重新設定影片描述，名稱和縮略圖';
+$lang['Get video description'] = '取得影片介紹';
+$lang['Get video tags'] = '取得影片標籤';
+$lang['This element is a video added with "Embedded Video"'] = '此元素是"嵌入式視頻增加影片"';
+$lang['Try in safe-mode'] = '嘗試在安全模式';
+$lang['Unable to contact host server'] = '無法聯繫主機服務器';
+$lang['Upload a new thumbnail'] = '上傳新的縮略圖';
+$lang['Use common setting'] = '使用常用的設定';
+$lang['Video URL'] = '影片網址';
+$lang['Add film effect'] = '新增底片效果';
+$lang['Author name'] = '作者姓名';
+$lang['Author portrait'] = '作者肖像';
+$lang['Autoplay'] = '自動播放';
+$lang['Change'] = '更改';
+$lang['Color'] = '顏色';
+$lang['Common configuration'] = '常用設定';
+$lang['Dailymotion player'] = '優酷播放器';
+$lang['In safe-mode, the plugin does\'t try to contact the video host, usefull on some websites where remote connections are blocked.'] = '在安全模式，插件不嘗試聯繫影像主機，可在一些網站上遠程連接被封鎖。';
+$lang['Logo'] = '標誌';
+$lang['No privacy option.'] = '沒有隱私選項。';
+$lang['Safe-mode'] = '安全模式';
+$lang['Show privacy details'] = '顯示隱私詳細資料';
+$lang['Supported services'] = '支持服務';
+$lang['The thumbnail was updated'] = '縮略圖已更新';
+?>
