b966ce3c1c51ee85194e92a875f54be72969057f
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-04-27 16:28:38 +0000

    [gvideo] Update et_EE, thanks to : Eggy
    git-svn-id: http://piwigo.org/svn/extensions/gvideo@22382 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/et_EE/plugin.lang.php
@@ -0,0 +1,34 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Get video description'] = 'Saa video kirjeldus';
+$lang['Get video tags'] = 'Saa video tag-id';
+$lang['Add a video'] = 'Lisa video';
+$lang['Add film effect'] = 'Lisa filmiefekt';
+$lang['Author name'] = 'Autori nimi';
+$lang['Author portrait'] = 'Autori pilt';
+$lang['Autoplay'] = 'Automaatesitlus';
+$lang['Change'] = 'Muuda';
+$lang['Changing the url will reset video description, name and thumbnail'] = 'Netiaadressi muutmisega algseadistatakse video kirjeldus, nimi ja pisipilt';
+$lang['Color'] = 'Värv';
+$lang['Common configuration'] = 'Üldised seadistused';
+?>
