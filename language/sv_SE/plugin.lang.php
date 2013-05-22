b0c1c9ba1ed5bd61e5097bcfd9aa25b061846fb4
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-05-22 22:44:54 +0000

    [embedded_videos] Update sv_SE, thanks to : stefan63
    git-svn-id: http://piwigo.org/svn/extensions/gvideo@22832 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/sv_SE/plugin.lang.php
@@ -0,0 +1,57 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['This element is a video added with "Embedded Video"'] = 'Det här elementet är en video tillagd med "Embedded Video"';
+$lang['Try in safe-mode'] = 'Försök i säkert läge';
+$lang['Unable to contact host server'] = 'Servern kunde inte nås';
+$lang['Upload a new thumbnail'] = 'Ladda upp en ny miniatyr (thumbnail)';
+$lang['Use common setting'] = 'Använd standard inställningar';
+$lang['Video URL'] = 'Filmens URL';
+$lang['Video properties'] = 'Filmens egenskaper';
+$lang['Video size'] = 'Filmens storlek';
+$lang['Video successfully added. <a href="%s">View</a>'] = 'Filmen uppladdad <a href="%s">View</a>';
+$lang['Video successfully updated'] = 'Filmen uppdaterad';
+$lang['Videos can be private if you use the private permalink.'] = 'Filmer kan göras privata om du använder permalänken privat.';
+$lang['Videos can be unlisted and private if the gallery website is within the authorized domains (PRO).'] = 'Filmer kan listas av och göras privata om albumets website fins innom de auktoriserade domänerna (PRO)';
+$lang['Videos can be unlisted but not private.'] = 'Filmer kan listas av men inte göras privata.';
+$lang['Vimeo player'] = 'Vimeo player';
+$lang['Add a video'] = 'Lägg till video';
+$lang['Add film effect'] = 'Lägg till film effekt';
+$lang['Author name'] = 'Utgivar namn';
+$lang['Author portrait'] = 'Om Utgivaren';
+$lang['Autoplay'] = 'Spela upp automatiskt';
+$lang['Change'] = 'Ändra';
+$lang['Changing the url will reset video description, name and thumbnail'] = 'Förändring av URL kommer att återställa beskrivning, namn, och miniatyr (Thumbnail)';
+$lang['Color'] = 'Färg';
+$lang['Common configuration'] = 'Allmän inställning';
+$lang['Dailymotion player'] = 'Uppspelare av dagens film';
+$lang['Get video description'] = 'Inhämta filmbeskrivning';
+$lang['Get video tags'] = 'Inhämta filmtaggar';
+$lang['In safe-mode, the plugin does\'t try to contact the video host, usefull on some websites where remote connections are blocked.'] = 'Plugginen försöker inte kontakta video källan i safe-mode, detta är användbart på vissa websidor som inte tillåter anslutningar utifrån (från andra URL).';
+$lang['Logo'] = 'Logotyp';
+$lang['No privacy option.'] = 'Ingen sekretess vald.';
+$lang['Please fill the video URL'] = 'Vänligen fyll i filmens URL';
+$lang['Safe-mode'] = 'Säkert-läge';
+$lang['Show privacy details'] = 'Visa sekretess information';
+$lang['Supported services'] = 'funktions stöd';
+$lang['The thumbnail was updated'] = 'miniatyr bilden (thumbnail) uppdaterad';
+?>
