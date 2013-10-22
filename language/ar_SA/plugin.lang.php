f988534a5ffbefb96bd2777af2db8e4ead42fbf9
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-10-22 17:02:39 +0000

    [embedded_videos] Update ar_SA, thanks to : aldambi
    git-svn-id: http://piwigo.org/svn/extensions/gvideo@25078 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/ar_SA/plugin.lang.php
@@ -0,0 +1,57 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['This element is a video added with "Embedded Video"'] = 'هذا العنصر هو فيديو واضيف مع "الفيديو المضمن"';
+$lang['Vimeo player'] = 'مشغل فيديو';
+$lang['Videos can be unlisted but not private.'] = 'اشرطة الفيديو يمكن ان تكون غير مدرجة في القائمة ولكن ليست مخصصة';
+$lang['Videos can be unlisted and private if the gallery website is within the authorized domains (PRO).'] = 'أشرطة الفيديو يمكن أن تكون غير مدرجة في القائمة ومخصصة في حال إذا كان موقع المعرض ضمن مجالات تحتاج أذن (PRO).';
+$lang['Videos can be private if you use the private permalink.'] = 'أشرطة الفيديو يمكن أن تكون خاصة إذا كنت تستخدم الرابط الثابت الخاص.';
+$lang['Video successfully updated'] = 'حدث الفيديو بنجاح';
+$lang['Video successfully added. <a href="%s">View</a>'] = 'اضيف الفيديو بنجاح.  <a href="%s">عرض</a>';
+$lang['Video size'] = 'حجم الفيديو';
+$lang['Video URL'] = 'رابط الفيديو';
+$lang['Video properties'] = 'خصائص الفيديو';
+$lang['Use common setting'] = 'استخدام الإعداد العام';
+$lang['Upload a new thumbnail'] = 'تحميل صورة مصغرة جديدة';
+$lang['Unable to contact host server'] = 'غير قادر على الاتصال بملقم المضيف';
+$lang['Try in safe-mode'] = 'جرب في الوضع الآمن';
+$lang['The thumbnail was updated'] = 'تم تحديث الصورة المصغرة';
+$lang['Show privacy details'] = 'عرض تفاصيل الخصوصية';
+$lang['Supported services'] = 'الخدمات المدعومة';
+$lang['Safe-mode'] = 'وضع آمن';
+$lang['Please fill the video URL'] = 'يرجى ملء URL الفيديو';
+$lang['In safe-mode, the plugin does\'t try to contact the video host, usefull on some websites where remote connections are blocked.'] = 'في الوضع الآمن، المكون الإضافي لن يحاول الاتصال بمضيف الفيديو، من المفيد في بعض المواقع حيث يتم حظر الاتصالات البعيدة.';
+$lang['No privacy option.'] = 'بلا خيارات خصوصية';
+$lang['Logo'] = 'شعار';
+$lang['Get video tags'] = 'الحصول على الكلمات الدليلية للفيديو';
+$lang['Get video description'] = 'الحصول على وصف الفيديو';
+$lang['Dailymotion player'] = 'مشغل ديلي موشن';
+$lang['Common configuration'] = 'التهيئة الشائعة';
+$lang['Color'] = 'اللون';
+$lang['Changing the url will reset video description, name and thumbnail'] = 'سيؤدي تغيير عنوان URL الى إعادة تعيين صف الفيديو، والاسم والصورة المصغرة';
+$lang['Change'] = 'تغيير';
+$lang['Autoplay'] = 'تشغيل تلقائي';
+$lang['Author portrait'] = 'صورة المؤلف ';
+$lang['Author name'] = 'اسم الكاتب';
+$lang['Add film effect'] = 'إضافة تأثير الفيلم';
+$lang['Add a video'] = 'إضافة فيديو';
+?>
