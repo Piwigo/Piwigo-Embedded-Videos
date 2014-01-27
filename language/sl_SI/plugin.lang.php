62d6fd737c947b22c6616ead8f83c976ec4a8d74
Piwigo-TranslationTeam <translate-github@piwigo.org>
2014-01-27 18:58:42 +0000

    [embedded_videos] Update sl_SI, thanks to : MarkoAm
    git-svn-id: http://piwigo.org/svn/extensions/gvideo@27013 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/sl_SI/plugin.lang.php
@@ -0,0 +1,57 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2014 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['This element is a video added with "Embedded Video"'] = 'Ta posnetek je bil dodan z "Embedded Video"';
+$lang['Try in safe-mode'] = 'Poskusite v varnem načinu';
+$lang['Unable to contact host server'] = 'Povezava z oddaljenim gostiteljem ni uspela';
+$lang['Upload a new thumbnail'] = 'Naloži novo prikazno sličico (thumbnail)';
+$lang['Use common setting'] = 'Uporabi skupne nastavitve';
+$lang['Video URL'] = 'URL naslov video posnetka';
+$lang['Video properties'] = 'LAstnosti video posnetka';
+$lang['Video size'] = 'Velikost posnetka';
+$lang['Video successfully added. <a href="%s">View</a>'] = 'Video posnetek je bil uspešno dodan <a href="%s">Poglej posnetek</a>';
+$lang['Video successfully updated'] = 'Video posnetek uspešno posodobljen';
+$lang['Videos can be private if you use the private permalink.'] = 'Video posnetki so lahko zasebni, če uporabite zasebno stalno povezavo (permalink)';
+$lang['Videos can be unlisted and private if the gallery website is within the authorized domains (PRO).'] = 'Video posnetki so lahko skriti in zasebni če je spletna stran galerije znotraj avtoriziranih domen. (PRO)';
+$lang['Videos can be unlisted but not private.'] = 'Video posnetki so lahko skriti vendar niso zasebni';
+$lang['Vimeo player'] = 'Vimeo predvajalnik';
+$lang['Add a video'] = 'Dodaj video';
+$lang['Add film effect'] = 'Dodaj učinek filma';
+$lang['Author name'] = 'Ime avtorja';
+$lang['Author portrait'] = 'Portret avtorja';
+$lang['Autoplay'] = 'Samodejno predvajanje';
+$lang['Change'] = 'Spremeni';
+$lang['Changing the url will reset video description, name and thumbnail'] = 'Sprememba URL-ja povzroči resetiranje imena, opisa in sličice posnetka.';
+$lang['Color'] = 'Barva';
+$lang['Common configuration'] = 'Skupne nastavitve';
+$lang['Dailymotion player'] = 'Dailymotion predvajalnik';
+$lang['Get video description'] = 'Pridobi opis video posnetka';
+$lang['Get video tags'] = 'Pridobi oznake video posnetka';
+$lang['In safe-mode, the plugin does\'t try to contact the video host, usefull on some websites where remote connections are blocked.'] = 'V varnem načinu vtičnik ne poskuša vzpostaviti povezave z gostiteljem. Uporabno na nakaterih spletnih mestih kjer je oddaljeno povezovanje onemogočeno';
+$lang['Logo'] = 'Logo';
+$lang['No privacy option.'] = 'Brez možnosti zasebnosti';
+$lang['Please fill the video URL'] = 'Prosimo vnesite URL naslov video posnetka';
+$lang['Safe-mode'] = 'Varni način';
+$lang['Show privacy details'] = 'Prikaži lastnosti zasebnosti';
+$lang['Supported services'] = 'Podprte storitve';
+$lang['The thumbnail was updated'] = 'Prikazna sličica je bila posodobljena';
+?>
