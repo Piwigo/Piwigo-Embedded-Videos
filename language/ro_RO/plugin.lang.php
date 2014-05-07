324cb6fee971e6e5a815f4e9f06ad05ff257aa37
Piwigo-TranslationTeam <translate-github@piwigo.org>
2014-05-07 10:09:58 +0000

    [embedded_videos] Update ro_RO, thanks to : cheilenereinfo
    git-svn-id: http://piwigo.org/svn/extensions/gvideo@28385 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/ro_RO/plugin.lang.php
@@ -0,0 +1,29 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2014 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Common configuration'] = 'Configuratia obisnuita';
+$lang['Color'] = 'Culoare';
+$lang['Author portrait'] = 'Portretul autorului';
+$lang['Author name'] = 'Numele autorului';
+$lang['Add video from hosting platform'] = 'Adauga video de pe o platforma de  gazduire';
+$lang['Add film effect'] = 'Adauga efectul de film';
+$lang['Add a video'] = 'Adauga un video';
