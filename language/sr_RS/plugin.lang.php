eaa8fd690749fe013c91e1c334b19e0e12833177
Piwigo-TranslationTeam <translate-github@piwigo.org>
2015-04-18 19:38:45 +0200

    Add sr_RS, thanks to gpopac

---
+++ language/sr_RS/plugin.lang.php
@@ -0,0 +1,63 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2015 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Video data like description and thumbnail might be missing'] = 'Можда недостају подаци о видеу као што је опис или умањени приказ';
+$lang['Video properties'] = 'Особине видеа';
+$lang['Video size'] = 'Величина видеа';
+$lang['Video successfully added. <a href="%s">View</a>'] = 'Видео је успешно додат. <a href="%s">Пусти</a>';
+$lang['Video successfully updated'] = 'Видео је успешно ажуриран';
+$lang['Videos'] = 'Видео снимци';
+$lang['Videos can be private if you use the private permalink.'] = 'Видео снимци могу бити приватни уколико котистите приватну везу до њих.';
+$lang['Videos can be unlisted and private if the gallery website is within the authorized domains (PRO).'] = 'Видео се не мора приказати или може бити приватан уколико је галерија на овом сајту овлашћена (ПРО).';
+$lang['Videos can be unlisted but not private.'] = 'Видео се не мора приказивати или може бити приватан.';
+$lang['Vimeo player'] = 'Вимео плејер';
+$lang['Width and height must be integers'] = 'Вредности ширини и висине морају бити целобројне';
+$lang['optional'] = 'опционо';
+$lang['Show privacy details'] = 'Прикажи детаље о приватности';
+$lang['Supported services'] = 'Подржани сервиси';
+$lang['The thumbnail was updated'] = 'Умањени приказ је ажуриран';
+$lang['This element is a video added with "Embedded Video"'] = 'Елемент је видео снимак додат помоћу опције „Угради видео“';
+$lang['This video was already registered'] = 'Овај видео снимак је већ регистрован';
+$lang['Unable to contact host server'] = 'Не могу да успоставим везу са сервером домаћином';
+$lang['Upload a new thumbnail'] = 'Пошаљи нову сличицу за умањени приказ';
+$lang['Use common setting'] = 'Користи уобичајене поставке';
+$lang['Video URL'] = 'Адреса видеа';
+$lang['Add a video'] = 'Додај видео';
+$lang['Add film effect'] = 'Додај филмски ефекат';
+$lang['Add video from embed code'] = 'Додај видео из кода за уграђивање';
+$lang['Add video from hosting platform'] = 'Додај видео са хостинга';
+$lang['Author name'] = 'Име аутора';
+$lang['Author portrait'] = 'Портрет аутора';
+$lang['Autoplay'] = 'Сам пусти видео';
+$lang['Change'] = 'Измени';
+$lang['Changing the url will reset video description, name and thumbnail'] = 'Промена адресе ће поништити опис, назив и умањени приказ видеа';
+$lang['Color'] = 'Боја';
+$lang['Common configuration'] = 'Уобичајена подешавања';
+$lang['Dailymotion player'] = 'Дејлимоушн плејер';
+$lang['Do not use this form for videos provided by Youtube, Dailymotion, Vimeo, Wat or Wideo.'] = 'Не користи форму видеа са ЈуТјуба, Дејлимоушна, Вимеа, Вата или Видеа.';
+$lang['Embed code'] = 'Код за уградњу';
+$lang['Get video description'] = 'Преузми опис видеа';
+$lang['Get video tags'] = 'Преузми ознаке видеа';
+$lang['Logo'] = 'Лого';
+$lang['No privacy option.'] = 'Нема опција приватности.';
+$lang['Please fill the embed code'] = 'Унесите код за уградњу';
+$lang['Please fill the video URL'] = 'Унесите адресу до видео снимка';
