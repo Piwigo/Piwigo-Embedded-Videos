27c58e013a0cdd1872d76af17c2d5c819f7913f1
Piwigo-TranslationTeam <translate-github@piwigo.org>
2022-04-16 03:34:26 +0200

    Update is_IS, thanks to gudjonsi

---
+++ language/is_IS/plugin.lang.php
@@ -0,0 +1,49 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Vimeo player'] = 'Vimeo spilari';
+$lang['Videos can be unlisted but not private.'] = 'Myndbönd geta verið óskráð en ekki lokuð.';
+$lang['Videos can be unlisted and private if the gallery website is within the authorized domains (PRO).'] = 'Myndbönd geta verið óskráð og lokuð ef vefsíða gallerísins er innan viðurkenndra léna (PRO).';
+$lang['Videos can be private if you use the private permalink.'] = 'Myndbönd geta verið persónuleg ef þú notar persónulega fasttengilinn.';
+$lang['Videos'] = 'Myndbönd';
+$lang['Video successfully updated'] = 'Myndband var uppfært';
+$lang['Video successfully added. <a href="%s">View</a>'] = 'Myndbandi var bætt við. <a href="%s">Skoða</a>';
+$lang['Video size'] = 'Myndbandastærð';
+$lang['Video properties'] = 'Myndbandaeiginleikar';
+$lang['Video data like description and thumbnail might be missing'] = 'Myndbandagögn eins og lýsing og smámynd gæti vantað';
+$lang['Video URL'] = 'Myndbandavefslóði';
+$lang['Use common setting'] = 'Notaðu algenga stillingu';
+$lang['Upload a new thumbnail'] = 'Hladdu upp nýrri smámynd';
+$lang['Unable to contact host server'] = 'Ekki hægt að hafa samband við hýsingarþjón';
+$lang['This video was already registered'] = 'Þetta myndband var þegar skráð';
+$lang['This element is a video added with "Embedded Video"'] = 'Þessi þáttur er myndband sem bætt var við með „Innfelldu myndbandi“';
+$lang['The thumbnail was updated'] = 'Smámyndin var uppfærð';
+$lang['Supported services'] = 'Studdar þjónustur';
+$lang['Show privacy details'] = 'Sýna friðhelgisupplýsingar';
+$lang['Reset thumbnail (download from host)'] = 'Endurstilla smámynd (hala niður frá hýsingaraðila)';
+$lang['optional'] = 'valfrjálst';
+$lang['Width and height must be integers'] = 'Breidd og hæð verða að vera heilar tölur';
+$lang['Please fill the video URL'] = 'Vinsamlegast fylltu út vefslóð myndbandsins';
+$lang['Please fill the embed code'] = 'Vinsamlegast fylltu inn innfellingarkóðann';
+$lang['No privacy option.'] = 'Enginn valkostur um friðhelgi.';
+$lang['Logo'] = 'Lógó';
+$lang['Get video tags'] = 'Sæktu myndbandsmerki';
+$lang['Get video description'] = 'Sækja lýsingu á myndbandi';
+$lang['Embed code'] = 'Fella inn kóða';
+$lang['Do not use this form for videos provided by Youtube, Dailymotion, Vimeo, Wat or Wideo.'] = 'Ekki nota þetta eyðublað fyrir myndbönd frá Youtube, Dailymotion, Vimeo, Wat eða Wideo.';
+$lang['Dailymotion player'] = 'Dailymotion spilari';
+$lang['Common configuration'] = 'Algeng uppsetning';
+$lang['Color'] = 'Litur';
+$lang['Changing the url will reset video description, name and thumbnail'] = 'Ef slóðinni er breytt þá endurstillast myndbandslýsing, nafn og smámynd';
+$lang['Change'] = 'Breyta';
+$lang['Autoplay'] = 'Sjálfvirk spilun';
+$lang['Author portrait'] = 'Portrett höfundar';
+$lang['Author name'] = 'Nafn höfundar';
+$lang['Add video from hosting platform'] = 'Bættu við myndbandi frá hýsingarumhverfi';
+$lang['Add video from embed code'] = 'Bættu við myndbandi úr innfellingarkóða';
+$lang['Add film effect'] = 'Bættu við kvikmyndaáhrifum';
+$lang['Add a video'] = 'Bættu við myndbandi';
