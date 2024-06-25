8bd803d55ce91ad57d8ecda148a7d36d827a2d62
Piwigo-TranslationTeam <translate-github@piwigo.org>
2024-06-25 06:44:15 +0200

    Update fr_CA, thanks to bgoutier

---
+++ language/fr_CA/plugin.lang.php
@@ -0,0 +1,12 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Upload a new thumbnail'] = 'Télécharger une nouvelle miniature';
+$lang['Videos can be private if you use the private permalink.'] = 'Les vidéos peuvent être privées si vous utilisez le permalink privé.';
+$lang['Get video tags'] = 'Récupérer les mots-clés';
+$lang['Please fill the embed code'] = 'Veuillez fournir le code d\'intégration';
+$lang['Please fill the video URL'] = 'Veuillez fournir l\'URL de la vidéo';
