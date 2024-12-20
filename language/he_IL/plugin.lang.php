d2dabef12b277148cbfd1ef65acd7769a7248e85
Piwigo-TranslationTeam <translate-github@piwigo.org>
2024-12-20 08:54:40 +0100

    Update he_IL, thanks to epifeny

---
+++ language/he_IL/plugin.lang.php
@@ -0,0 +1,49 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Width and height must be integers'] = 'רוחב וגובה חייבים להיות מספרים שלמים';
+$lang['optional'] = 'אופציונלי';
+$lang['Reset thumbnail (download from host)'] = 'איפוס תמונה ממארח';
+$lang['Show privacy details'] = 'הצג פרטי פרטיות';
+$lang['Supported services'] = 'שירותים נתמכים';
+$lang['The thumbnail was updated'] = 'התמונה התעדכנה';
+$lang['This element is a video added with "Embedded Video"'] = 'הרכיב הזה הוא וידאו שנוסף עם "וידאו מוטבע"';
+$lang['This video was already registered'] = 'הווידאו הזה כבר נרשם';
+$lang['Unable to contact host server'] = 'לא ניתן ליצור קשר עם שרת המארח';
+$lang['Upload a new thumbnail'] = 'העלה תמונה חדשה';
+$lang['Use common setting'] = 'השתמש בהגדרה רגילה';
+$lang['Video URL'] = 'כתובת URL של הווידאו';
+$lang['Video data like description and thumbnail might be missing'] = 'נתוני הווידאו כמו תיאור ותמונה עשויים להיות חסרים';
+$lang['Video properties'] = 'מאפייני הווידאו';
+$lang['Video size'] = 'גודל הווידאו';
+$lang['Video successfully added. <a href="%s">View</a>'] = 'הווידאו נוסף בהצלחה. <a href="%s">צפה</a>';
+$lang['Video successfully updated'] = 'הווידאו עודכן בהצלחה';
+$lang['Videos'] = 'וידאו';
+$lang['Videos can be private if you use the private permalink.'] = 'הווידאו יכול להיות פרטי אם אתה משתמש בקישור פרטי.';
+$lang['Videos can be unlisted and private if the gallery website is within the authorized domains (PRO).'] = 'הווידאו יכול להיות לא רשום ופרטי אם אתר הגלריה נמצא בתחום התחולה (PRO).';
+$lang['Videos can be unlisted but not private.'] = 'הווידאו יכול להיות לא רשום אך לא פרטי';
+$lang['Vimeo player'] = 'נגן Vimeo';
+$lang['Add a video'] = 'הוסף וידאו';
+$lang['Add film effect'] = 'הוסף אפקט סרט';
+$lang['Add video from embed code'] = 'הוסף וידאו מקוד הטמעה';
+$lang['Add video from hosting platform'] = 'הוסף וידאו מפלטפורמת אחסון';
+$lang['Author name'] = 'שם המחבר';
+$lang['Author portrait'] = 'תמונת מחבר';
+$lang['Autoplay'] = 'הפעלה אוטומטית';
+$lang['Change'] = 'שינוי';
+$lang['Changing the url will reset video description, name and thumbnail'] = 'שינוי ה-URL יאפס את תיאור הוידאו, השם והתמונה הממוזערת';
+$lang['Color'] = 'צבע';
+$lang['Common configuration'] = 'הגדרה רגילה';
+$lang['Dailymotion player'] = 'נגן Dailymotion';
+$lang['Do not use this form for videos provided by Youtube, Dailymotion, Vimeo, Wat or Wideo.'] = 'אל תשתמש בטופס זה לוידאו שסופק על ידי Youtube, Dailymotion, Vimeo, Wat או Wideo.';
+$lang['Embed code'] = 'קוד הטמעה';
+$lang['Get video description'] = 'קבל תיאור הוידאו';
+$lang['Get video tags'] = 'קבל תגיות הוידאו';
+$lang['Logo'] = 'לוגו';
+$lang['No privacy option.'] = 'אין אפשרות לפרטיות.';
+$lang['Please fill the embed code'] = 'אנא מלא את קוד ההטמעה';
+$lang['Please fill the video URL'] = 'אנא מלא את כתובת ה-URL של הוידאו';
